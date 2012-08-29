f5d981ab4d6de03965bcdd28042b78e425b2c3ee
Chris Lu <chris.lu@gmail.com>
2012-08-29 01:42:24 -0700

    scaffold for tests

---
+++ weed-fs/src/pkg/topology/topo_test.go
@@ -0,0 +1,41 @@
+package topology
+
+import (
+    "testing"
+)
+
+func TestAddVolume(t *testing.T) {
+
+}
+
+func TestAddServer(t *testing.T) {
+
+}
+
+func TestAddRack(t *testing.T) {
+
+}
+
+func TestAddDataCenter(t *testing.T) {
+
+}
+
+func TestReserveVolume(t *testing.T) {
+
+}
+
+func TestRemoveVolume(t *testing.T) {
+
+}
+
+func TestRemoveServer(t *testing.T) {
+
+}
+
+func TestRemoveRack(t *testing.T) {
+
+}
+
+func TestRemoveDataCenter(t *testing.T) {
+
+}
