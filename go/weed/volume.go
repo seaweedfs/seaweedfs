c6bd4e656edaf92c8a2e44f196db156d99f96f20
Chris Lu <chris.lu@gmail.com>
2013-07-13 23:46:12 -0700

    add support for http://localhost:8080/vid/key_cookie or http://localhost:8080/vid/key_cookie.txt

--- go/weed/volume.go
+++ go/weed/volume.go
@@ -266,11 +266,20 @@ func DeleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func parseURLPath(path string) (vid, fid, filename, ext string) {
-	if strings.Count(path, "/") == 3 {
+	switch strings.Count(path, "/") {
+	case 3:
 		parts := strings.Split(path, "/")
 		vid, fid, filename = parts[1], parts[2], parts[3]
 		ext = filepath.Ext(filename)
-	} else {
+	case 2:
+    parts := strings.Split(path, "/")
+    vid, fid = parts[1], parts[2]
+    dotIndex := strings.LastIndex(fid, ".")
+    if dotIndex > 0 {
+      ext = fid[dotIndex:]
+      fid = fid[0:dotIndex]
+    }
+	default:
 		sepIndex := strings.LastIndex(path, "/")
 		commaIndex := strings.LastIndex(path[sepIndex:], ",")
 		if commaIndex <= 0 {
