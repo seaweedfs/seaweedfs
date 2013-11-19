1645d3c18534324de795643974299de0d512d3f1
Chris Lu <chris.lu@gmail.com>
2013-11-19 02:12:56 -0800

    avoid nil cases when error

--- go/operation/submit.go
+++ go/operation/submit.go
@@ -132,7 +132,10 @@ func upload_one_chunk(filename string, reader io.Reader, master, replication str
 	fileUrl, fid := "http://"+ret.PublicUrl+"/"+ret.Fid, ret.Fid
 	glog.V(4).Info("Uploading part ", filename, " to ", fileUrl, "...")
 	uploadResult, uploadError := Upload(fileUrl, filename, reader, false, "application/octet-stream")
-	return fid, uploadResult.Size, uploadError
+	if uploadError != nil {
+		return fid, 0, uploadError
+	}
+	return fid, uploadResult.Size, nil
 }
 
 func upload_file_id_list(fileUrl, filename string, fids []string) error {
