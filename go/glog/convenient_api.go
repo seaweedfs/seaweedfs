161526555ff77faf2c6af085619a591cf8436f58
Chris Lu <chris.lu@gmail.com>
2014-04-13 10:20:43 -0700

    remove unused code additional functions for glog

--- go/glog/convenient_api.go
+++ go/glog/convenient_api.go
@@ -4,13 +4,5 @@ import ()
 
 /*
 Copying the original glog because it is missing several convenient methods.
-1. use ToStderrAndLog() in the weed.go
-2. remove nano time in log format
+1. remove nano time in log format
 */
-
-func ToStderr() {
-	logging.toStderr = true
-}
-func ToStderrAndLog() {
-	logging.alsoToStderr = true
-}
