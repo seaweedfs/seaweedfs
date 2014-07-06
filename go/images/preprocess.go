413e5c145cd73693d2bda1b3038ce644ab71248a
Chris Lu <chris.lu@gmail.com>
2014-07-05 18:01:17 -0700

    lowercase when checking file name extensions.

--- go/images/preprocess.go
+++ go/images/preprocess.go
@@ -2,6 +2,7 @@ package images
 
 import (
 	"path/filepath"
+	"strings"
 )
 
 /*
@@ -10,15 +11,16 @@ import (
 * 2. resize the image to a width or height limit
 * 3. remove the exif data
 * Call this function on any file uploaded to weedfs
-* 
-*/
+*
+ */
 func MaybePreprocessImage(filename string, data []byte, width, height int) (resized []byte, w int, h int) {
 	ext := filepath.Ext(filename)
+	ext = strings.ToLower(ext)
 	switch ext {
 	case ".png", ".gif":
 		return Resized(ext, data, width, height)
 	case ".jpg", ".jpeg":
-	  data = FixJpgOrientation(data)
+		data = FixJpgOrientation(data)
 		return Resized(ext, data, width, height)
 	}
 	return data, 0, 0
