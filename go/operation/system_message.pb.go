2f10f2960b30cff9d5d32d2d938232ba5d7ba519
yanyiwu <i@yanyiwu.com>
2015-01-30 15:09:37 +0800

    use proto "github.com/golang/protobuf/proto"

--- go/operation/system_message.pb.go
+++ go/operation/system_message.pb.go
@@ -14,7 +14,7 @@ It has these top-level messages:
 */
 package operation
 
-import proto "code.google.com/p/goprotobuf/proto"
+import proto "github.com/golang/protobuf/proto"
 import math "math"
 
 // Reference imports to suppress errors if they are not otherwise used.
