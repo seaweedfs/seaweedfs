c37a20178e76661acacbf4e84da0a916d9129cbc
chrislusf <chris.lu@gmail.com>
2015-03-29 14:22:54 -0700

    Adjust logging level.

--- go/weed/weed_server/volume_server.go
+++ go/weed/weed_server/volume_server.go
@@ -77,7 +77,7 @@ func NewVolumeServer(adminMux, publicMux *http.ServeMux, ip string,
 					glog.V(0).Infoln("Volume Server Connected with master at", master)
 				}
 			} else {
-				glog.V(0).Infof("Volume Server Failed to talk with master %+v: %v", vs, err)
+				glog.V(1).Infof("Volume Server Failed to talk with master %+v: %v", vs, err)
 				if connected {
 					connected = false
 				}
