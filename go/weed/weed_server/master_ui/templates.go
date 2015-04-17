3f253a587ccf9532a530d34505bbff47f4fde170
yanyiwu <i@yanyiwu.com>
2015-04-18 02:26:27 +0800

    [ui] seaweedfs logo and icon

--- go/weed/weed_server/master_ui/templates.go
+++ go/weed/weed_server/master_ui/templates.go
@@ -8,12 +8,16 @@ var StatusTpl = template.Must(template.New("status").Parse(`<!DOCTYPE html>
 <html>
   <head>
     <title>SeaweedFS {{ .Version }}</title>
+	<link rel="icon" href="http://7viirv.com1.z0.glb.clouddn.com/seaweed_favicon.png" sizes="32x32" />  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
   </head>
   <body>
     <div class="container">
       <div class="page-header">
-        <h1>SeaweedFS <small>{{ .Version }}</small></h1>
+	    <h1>
+	      <img src="http://7viirv.com1.z0.glb.clouddn.com/seaweed50x50.png"></img>
+          SeaweedFS <small>{{ .Version }}</small>
+	    </h1>
       </div>
 
       <div class="row">
