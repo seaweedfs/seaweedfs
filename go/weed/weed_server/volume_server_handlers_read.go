3fb98a904bf5d67e7f51797fbee0aa7ddffb5903
chrislusf <chris.lu@gmail.com>
2016-04-15 11:04:39 -0700

    fmt

--- go/weed/weed_server/volume_server_handlers_read.go
+++ go/weed/weed_server/volume_server_handlers_read.go
@@ -143,8 +143,8 @@ func (vs *VolumeServer) GetOrHeadHandler(w http.ResponseWriter, r *http.Request)
 func (vs *VolumeServer) FaviconHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := images.Asset("favicon/favicon.ico")
 	if err != nil {
-	    glog.V(2).Infoln("favicon read error:", err)
-	    return
+		glog.V(2).Infoln("favicon read error:", err)
+		return
 	}
 
 	if e := writeResponseContent("favicon.ico", "image/x-icon", bytes.NewReader(data), w, r); e != nil {
