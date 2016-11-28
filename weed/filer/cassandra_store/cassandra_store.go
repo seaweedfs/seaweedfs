5ee7fdc0cc424b508bec85fccab671285359dd2e
eshujiushiwo <378013446@qq.com>
2016-11-28 16:03:39 +0800

    fix hosts strings
    fix hosts strings

--- weed/filer/cassandra_store/cassandra_store.go
+++ weed/filer/cassandra_store/cassandra_store.go
@@ -30,13 +30,13 @@ type CassandraStore struct {
 	session *gocql.Session
 }
 
-func NewCassandraStore(keyspace string, hosts ...string) (c *CassandraStore, err error) {
+func NewCassandraStore(keyspace string, hosts string) (c *CassandraStore, err error) {
 	c = &CassandraStore{}
 	        s := strings.Split(hosts, ",")
         if len(s) == 1 {
 
 		glog.V(2).Info("Only one  cassandra node to connect!A Cluster is Proposed!Now using:", string(hosts))
-                c.cluster = gocql.NewCluster(hosts...)
+                c.cluster = gocql.NewCluster(hosts)
         } else if len(s) > 1 {
                 c.cluster = gocql.NewCluster(s[0], s[1])
         }
