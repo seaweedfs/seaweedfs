4936d6c342e16edd9ee37d2b2ec2c890287670d4
Chris Lu <chris.lu@gmail.com>
2018-05-07 00:37:47 -0700

    adjusting for FUSE

--- weed/command/mount_std.go
+++ weed/command/mount_std.go
@@ -22,7 +22,21 @@ func runMount(cmd *Command, args []string) bool {
 
 	fuse.Unmount(*mountOptions.dir)
 
-	c, err := fuse.Mount(*mountOptions.dir, fuse.LocalVolume())
+	c, err := fuse.Mount(
+		*mountOptions.dir,
+		fuse.VolumeName("SeaweedFS"),
+		fuse.FSName("SeaweedFS"),
+		fuse.NoAppleDouble(),
+		fuse.NoAppleXattr(),
+		fuse.ExclCreate(),
+		fuse.DaemonTimeout("3600"),
+		fuse.AllowOther(),
+		fuse.AllowSUID(),
+		fuse.DefaultPermissions(),
+		// fuse.MaxReadahead(1024*128), // TODO: not tested yet, possibly improving read performance
+		fuse.AsyncRead(),
+		fuse.WritebackCache(),
+	)
 	if err != nil {
 		glog.Fatal(err)
 		return false
