78aabf66cefe6dcbfd700b7098c81d5daef46969
Chris Lu <chris.lu@gmail.com>
2018-05-29 23:46:17 -0700

    add error details

--- weed/server/filer_server_handlers_admin.go
+++ weed/server/filer_server_handlers_admin.go
@@ -8,6 +8,7 @@ import (
 	"github.com/chrislusf/seaweedfs/weed/pb/filer_pb"
 	"strconv"
 	"time"
+	"fmt"
 )
 
 func (fs *FilerServer) registerHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,19 +17,19 @@ func (fs *FilerServer) registerHandler(w http.ResponseWriter, r *http.Request) {
 	fileSize, err := strconv.ParseUint(r.FormValue("fileSize"), 10, 64)
 	if err != nil {
 		glog.V(0).Infof("register %s to %s parse fileSize %s: %v", fileId, path, r.FormValue("fileSize"), err)
-		writeJsonError(w, r, http.StatusInternalServerError, err)
+		writeJsonError(w, r, http.StatusInternalServerError, fmt.Errorf("parsing fileSize: %v", err))
 		return
 	}
 	uid, err := strconv.ParseUint(r.FormValue("uid"), 10, 64)
 	if err != nil && r.FormValue("uid") != "" {
 		glog.V(0).Infof("register %s to %s parse uid %s: %v", fileId, path, r.FormValue("uid"), err)
-		writeJsonError(w, r, http.StatusInternalServerError, err)
+		writeJsonError(w, r, http.StatusInternalServerError, fmt.Errorf("parsing uid: %v", err))
 		return
 	}
 	gid, err := strconv.ParseUint(r.FormValue("gid"), 10, 64)
 	if err != nil && r.FormValue("gid") != "" {
 		glog.V(0).Infof("register %s to %s parse gid %s: %v", fileId, path, r.FormValue("gid"), err)
-		writeJsonError(w, r, http.StatusInternalServerError, err)
+		writeJsonError(w, r, http.StatusInternalServerError, fmt.Errorf("parsing gid: %v", err))
 		return
 	}
 	entry := &filer2.Entry{
@@ -50,7 +51,7 @@ func (fs *FilerServer) registerHandler(w http.ResponseWriter, r *http.Request) {
 	err = fs.filer.CreateEntry(entry)
 	if err != nil {
 		glog.V(0).Infof("register %s to %s error: %v", fileId, path, err)
-		writeJsonError(w, r, http.StatusInternalServerError, err)
+		writeJsonError(w, r, http.StatusInternalServerError, fmt.Errorf("create %s: %v", path, err))
 	} else {
 		w.WriteHeader(http.StatusOK)
 	}
