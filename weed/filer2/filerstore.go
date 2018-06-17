14c6f12b0c8c8321a267a2e7cf9724f8f30f2104
Chris Lu <chris.lu@gmail.com>
2018-06-17 13:24:57 -0700

    adjust interface parameter name and doc

--- weed/filer2/filerstore.go
+++ weed/filer2/filerstore.go
@@ -5,6 +5,7 @@ import (
 )
 
 type FilerStore interface {
+	// GetName gets the name to locate the configuration in filer.toml file
 	GetName() string
 	// Initialize initializes the file store
 	Initialize(configuration Configuration) error
@@ -12,7 +13,7 @@ type FilerStore interface {
 	UpdateEntry(*Entry) (err error)
 	FindEntry(FullPath) (entry *Entry, err error)
 	DeleteEntry(FullPath) (err error)
-	ListDirectoryEntries(dirPath FullPath, startFileName string, inclusive bool, limit int) ([]*Entry, error)
+	ListDirectoryEntries(dirPath FullPath, startFileName string, includeStartFile bool, limit int) ([]*Entry, error)
 }
 
 var ErrNotFound = errors.New("filer: no entry is found in filer store")
