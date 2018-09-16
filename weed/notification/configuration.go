bc03233364484653228066d9ea27b6e78ba640f6
Chris Lu <chris.lu@gmail.com>
2018-09-16 01:37:35 -0700

    refactoring

--- weed/notification/configuration.go
+++ weed/notification/configuration.go
@@ -3,7 +3,17 @@ package notification
 import (
 	"github.com/chrislusf/seaweedfs/weed/glog"
 	"github.com/spf13/viper"
+	"github.com/chrislusf/seaweedfs/weed/util"
+	"github.com/golang/protobuf/proto"
 )
+
+type MessageQueue interface {
+	// GetName gets the name to locate the configuration in filer.toml file
+	GetName() string
+	// Initialize initializes the file store
+	Initialize(configuration util.Configuration) error
+	SendMessage(key string, message proto.Message) error
+}
 
 var (
 	MessageQueues []MessageQueue
