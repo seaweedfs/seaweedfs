a8cd7df44ae5d3bea4b0c9ce3d3ab71c7b3cc60c
Chris Lu <chris.lu@gmail.com>
2018-09-20 09:00:15 -0700

    let filer choose default replication

--- weed/command/mount.go
+++ weed/command/mount.go
@@ -31,7 +31,7 @@ func init() {
 	mountOptions.dir = cmdMount.Flag.String("dir", ".", "mount weed filer to this directory")
 	mountOptions.dirListingLimit = cmdMount.Flag.Int("dirListLimit", 100000, "limit directory listing size")
 	mountOptions.collection = cmdMount.Flag.String("collection", "", "collection to create the files")
-	mountOptions.replication = cmdMount.Flag.String("replication", "000", "replication to create to files")
+	mountOptions.replication = cmdMount.Flag.String("replication", "", "replication(e.g. 000, 001) to create to files. If empty, let filer decide.")
 	mountOptions.ttlSec = cmdMount.Flag.Int("ttl", 0, "file ttl in seconds")
 	mountOptions.chunkSizeLimitMB = cmdMount.Flag.Int("chunkSizeLimitMB", 16, "local write buffer size, also chunk large files")
 	mountOptions.dataCenter = cmdMount.Flag.String("dataCenter", "", "prefer to write to the data center")
