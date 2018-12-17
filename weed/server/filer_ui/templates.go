52712782a215d2e4eebcc8de07dde2b08f7c61c1
Chris Lu <chris.lu@gmail.com>
2018-12-17 00:31:23 -0800

    adding link

--- weed/server/filer_ui/templates.go
+++ weed/server/filer_ui/templates.go
@@ -43,7 +43,7 @@ var StatusTpl = template.Must(template.New("status").Funcs(funcMap).Parse(`<!DOC
 	<div class="container">
 		<div class="page-header">
 			<h1>
-				<img src="/seaweedfsstatic/seaweed50x50.png"></img>
+				<a href="https://github.com/chrislusf/seaweedfs"><img src="/seaweedfsstatic/seaweed50x50.png"></img></a>
 				SeaweedFS Filer
 			</h1>
 		</div>
