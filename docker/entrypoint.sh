469afddc80ce7af45e2415c21798bbd3f41cd599
Chris Lu <chris.lu@gmail.com>
2018-12-27 14:28:28 -0800

    use /data for filer

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -29,7 +29,6 @@ case "$1" in
   	;;
 
   'filer')
-  	exec mkdir /data/filerdb
   	ARGS="-ip `hostname -i` "
   	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
 		ARGS="$ARGS -master=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
