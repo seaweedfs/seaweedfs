09471b46f90590cb1dede387a967f92c2250b9a1
Chris Lu <chris.lu@gmail.com>
2019-01-26 00:10:29 -0600

    fix related tests

--- weed/storage/needle/compact_map_test.go
+++ weed/storage/needle/compact_map_test.go
@@ -68,7 +68,7 @@ func TestCompactMap(t *testing.T) {
 				t.Fatal("key", i, "size", v.Size)
 			}
 		} else if i%37 == 0 {
-			if ok && v.Size > 0 {
+			if ok && v.Size != TombstoneFileSize {
 				t.Fatal("key", i, "should have been deleted needle value", v)
 			}
 		} else if i%2 == 0 {
@@ -81,7 +81,7 @@ func TestCompactMap(t *testing.T) {
 	for i := uint32(10 * batch); i < 100*batch; i++ {
 		v, ok := m.Get(NeedleId(i))
 		if i%37 == 0 {
-			if ok && v.Size > 0 {
+			if ok && v.Size != TombstoneFileSize {
 				t.Fatal("key", i, "should have been deleted needle value", v)
 			}
 		} else if i%2 == 0 {
