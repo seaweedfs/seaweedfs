221105eea3cb2cfb587870df0fe0e62c640c4b99
Chris Lu <chris.lu@gmail.com>
2019-01-28 11:46:46 -0800

    Revert "use the first entry to bootstrap master cluster"
    This reverts commit 40c8725ffa02767344184fe952f7799fe4250ef9.

--- weed/server/raft_server.go
+++ weed/server/raft_server.go
@@ -3,6 +3,7 @@ package weed_server
 import (
 	"encoding/json"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"path"
 	"reflect"
@@ -70,8 +71,8 @@ func NewRaftServer(r *mux.Router, peers []string, httpAddr string, dataDir strin
 	for _, peer := range s.peers {
 		s.raftServer.AddPeer(peer, "http://"+peer)
 	}
-
-	if s.raftServer.IsLogEmpty() && isTheFirstOne(httpAddr, s.peers) {
+	time.Sleep(time.Duration(1000+rand.Int31n(3000)) * time.Millisecond)
+	if s.raftServer.IsLogEmpty() {
 		// Initialize the server by joining itself.
 		glog.V(0).Infoln("Initializing new cluster")
 
@@ -127,12 +128,4 @@ func isPeersChanged(dir string, self string, peers []string) (oldPeers []string,
 
 	return oldPeers, !reflect.DeepEqual(peers, oldPeers)
 
-}
-
-func isTheFirstOne(self string, peers []string) bool {
-	sort.Strings(peers)
-	if len(peers)<=0{
-		return true
-	}
-	return self == peers[0]
 }
