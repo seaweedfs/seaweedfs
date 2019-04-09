a55e708d36c31973a7d117f8ef1d4f36cd382801
Chris Lu <chris.lu@gmail.com>
2019-04-09 00:18:32 -0700

    weed volume: disable block cache on leveldb
    saving 8MB for each volume, but at the cost of slower (offset,size) lookup

--- weed/filer2/leveldb/leveldb_store.go
+++ weed/filer2/leveldb/leveldb_store.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/syndtr/goleveldb/leveldb/opt"
 
 	"github.com/chrislusf/seaweedfs/weed/filer2"
 	"github.com/chrislusf/seaweedfs/weed/glog"
@@ -39,7 +40,9 @@ func (store *LevelDBStore) initialize(dir string) (err error) {
 		return fmt.Errorf("Check Level Folder %s Writable: %s", dir, err)
 	}
 
-	if store.db, err = leveldb.OpenFile(dir, nil); err != nil {
+	if store.db, err = leveldb.OpenFile(dir, &opt.Options{
+		BlockCacheCapacity: -1, // default value is 8MiB
+	}); err != nil {
 		glog.Infof("filer store open dir %s: %v", dir, err)
 		return
 	}
