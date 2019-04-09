784c5bb73a71bad615e500e4254c11e736b99a41
Chris Lu <chris.lu@gmail.com>
2019-04-08 22:01:29 -0700

    add build option support 5-byte offset

--- weed/storage/types/needle_types.go
+++ weed/storage/types/needle_types.go
@@ -12,10 +12,6 @@ type Offset struct {
 	OffsetLower
 }
 
-type OffsetHigher struct {
-	// b4 byte
-}
-
 type OffsetLower struct {
 	b3 byte
 	b2 byte
@@ -26,12 +22,10 @@ type OffsetLower struct {
 type Cookie uint32
 
 const (
-	OffsetSize            = 4 // + 1
 	SizeSize              = 4 // uint32 size
 	NeedleEntrySize       = CookieSize + NeedleIdSize + SizeSize
 	TimestampSize         = 8 // int64 size
 	NeedlePaddingSize     = 8
-	MaxPossibleVolumeSize = 4 * 1024 * 1024 * 1024 * 8
 	TombstoneFileSize     = math.MaxUint32
 	CookieSize            = 4
 )
@@ -53,51 +47,4 @@ func ParseCookie(cookieString string) (Cookie, error) {
 		return 0, fmt.Errorf("needle cookie %s format error: %v", cookieString, err)
 	}
 	return Cookie(cookie), nil
-}
-
-func OffsetToBytes(bytes []byte, offset Offset) {
-	bytes[3] = offset.b0
-	bytes[2] = offset.b1
-	bytes[1] = offset.b2
-	bytes[0] = offset.b3
-}
-
-// only for testing, will be removed later.
-func Uint32ToOffset(offset uint32) Offset {
-	return Offset{
-		OffsetLower: OffsetLower{
-			b0: byte(offset),
-			b1: byte(offset >> 8),
-			b2: byte(offset >> 16),
-			b3: byte(offset >> 24),
-		},
-	}
-}
-
-func BytesToOffset(bytes []byte) Offset {
-	return Offset{
-		OffsetLower: OffsetLower{
-			b0: bytes[3],
-			b1: bytes[2],
-			b2: bytes[1],
-			b3: bytes[0],
-		},
-	}
-}
-
-func (offset Offset) IsZero() bool {
-	return offset.b0 == 0 && offset.b1 == 0 && offset.b2 == 0 && offset.b3 == 0
-}
-
-func ToOffset(offset int64) Offset {
-	smaller := uint32(offset / int64(NeedlePaddingSize))
-	return Uint32ToOffset(smaller)
-}
-
-func (offset Offset) ToAcutalOffset() (actualOffset int64) {
-	return (int64(offset.b0) + int64(offset.b1)<<8 + int64(offset.b2)<<16 + int64(offset.b3)<<24) * int64(NeedlePaddingSize)
-}
-
-func (offset Offset) String() string {
-	return fmt.Sprintf("%d", int64(offset.b0)+int64(offset.b1)<<8+int64(offset.b2)<<16+int64(offset.b3)<<24)
 }
---
+++ weed/storage/types/offset_4bytes.go
@@ -0,0 +1,63 @@
+// +build !5BytesOffset
+
+package types
+
+import (
+	"fmt"
+)
+
+type OffsetHigher struct {
+	// b4 byte
+}
+
+const (
+	OffsetSize            = 4
+	MaxPossibleVolumeSize = 4 * 1024 * 1024 * 1024 * 8 // 32GB
+)
+
+func OffsetToBytes(bytes []byte, offset Offset) {
+	bytes[3] = offset.b0
+	bytes[2] = offset.b1
+	bytes[1] = offset.b2
+	bytes[0] = offset.b3
+}
+
+// only for testing, will be removed later.
+func Uint32ToOffset(offset uint32) Offset {
+	return Offset{
+		OffsetLower: OffsetLower{
+			b0: byte(offset),
+			b1: byte(offset >> 8),
+			b2: byte(offset >> 16),
+			b3: byte(offset >> 24),
+		},
+	}
+}
+
+func BytesToOffset(bytes []byte) Offset {
+	return Offset{
+		OffsetLower: OffsetLower{
+			b0: bytes[3],
+			b1: bytes[2],
+			b2: bytes[1],
+			b3: bytes[0],
+		},
+	}
+}
+
+func (offset Offset) IsZero() bool {
+	return offset.b0 == 0 && offset.b1 == 0 && offset.b2 == 0 && offset.b3 == 0
+}
+
+func ToOffset(offset int64) Offset {
+	smaller := uint32(offset / int64(NeedlePaddingSize))
+	return Uint32ToOffset(smaller)
+}
+
+func (offset Offset) ToAcutalOffset() (actualOffset int64) {
+	return (int64(offset.b0) + int64(offset.b1)<<8 + int64(offset.b2)<<16 + int64(offset.b3)<<24) * int64(NeedlePaddingSize)
+}
+
+func (offset Offset) String() string {
+	return fmt.Sprintf("%d", int64(offset.b0)+int64(offset.b1)<<8+int64(offset.b2)<<16+int64(offset.b3)<<24)
+}
