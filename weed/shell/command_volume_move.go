d06d3c57d4afec647015cf96bfc57035fe784101
Chris Lu <chris.lu@gmail.com>
2019-04-20 20:12:07 -0700

    add notes

--- weed/shell/command_volume_move.go
+++ weed/shell/command_volume_move.go
@@ -25,7 +25,7 @@ func (c *commandVolumeMove) Name() string {
 }
 
 func (c *commandVolumeMove) Help() string {
-	return `move a live volume from one volume server to another volume server
+	return `<experimental> move a live volume from one volume server to another volume server
 
 	volume.move <source volume server host:port> <target volume server host:port> <volume id>
 
