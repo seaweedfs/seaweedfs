ca2b81a944b995fd7ffafdb7e0bf6eb8df1ecdcb
Chris Lu <chris.lu@gmail.com>
2019-07-31 02:09:04 -0700

    fix test error

--- weed/server/master_grpc_server.go
+++ weed/server/master_grpc_server.go
@@ -249,7 +249,7 @@ func (ms *MasterServer) KeepConnected(stream master_pb.Seaweed_KeepConnectedServ
 func (ms *MasterServer) informNewLeader(stream master_pb.Seaweed_KeepConnectedServer) error {
 	leader, err := ms.Topo.Leader()
 	if err != nil {
-		glog.Error("topo leader: %v", err)
+		glog.Errorf("topo leader: %v", err)
 		return raft.NotLeaderError
 	}
 	if err := stream.Send(&master_pb.VolumeLocation{
