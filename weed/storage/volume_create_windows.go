5a6dfb05fc0d248f896aa7f341f55a257a5b58f8
j.laycock <joseph.laycock@4sightimaging.com>
2019-09-05 11:46:12 +0100

    Only use truncate flag when creating non memory map files, rename and use memory map size for reserving max file size

--- weed/storage/volume_create_windows.go
+++ weed/storage/volume_create_windows.go
@@ -12,22 +12,26 @@ import (
 	"github.com/joeslay/seaweedfs/weed/os_overloads"
 )
 
-func createVolumeFile(fileName string, preallocate int64, useMemoryMap uint32) (*os.File, error) {
+func createVolumeFile(fileName string, preallocate int64, memoryMapSizeMB uint32) (*os.File, error) {
 
 	mMap, exists := memory_map.FileMemoryMap[fileName]
 	if !exists {
-		file, e := os_overloads.OpenFile(fileName, windows.O_RDWR|windows.O_CREAT, 0644, useMemoryMap > 0)
-		if useMemoryMap > 0 {
-			memory_map.FileMemoryMap[fileName] = new(memory_map.MemoryMap)
-
-			new_mMap := memory_map.FileMemoryMap[fileName]
-			new_mMap.CreateMemoryMap(file, 1024*1024*1024*2)
-		}
 
 		if preallocate > 0 {
 			glog.V(0).Infof("Preallocated disk space for %s is not supported", fileName)
 		}
-		return file, e
+
+		if memoryMapSizeMB > 0 {
+			file, e := os_overloads.OpenFile(fileName, windows.O_RDWR|windows.O_CREAT, 0644, true)
+			memory_map.FileMemoryMap[fileName] = new(memory_map.MemoryMap)
+
+			new_mMap := memory_map.FileMemoryMap[fileName]
+			new_mMap.CreateMemoryMap(file, 1024*1024*uint64(memoryMapSizeMB))
+			return file, e
+		} else {
+			file, e := os_overloads.OpenFile(fileName, windows.O_RDWR|windows.O_CREAT|windows.O_TRUNC, 0644, false)
+			return file, e
+		}
 	} else {
 		return mMap.File, nil
 	}
