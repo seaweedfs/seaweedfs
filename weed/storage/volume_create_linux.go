6526fb70243a2d1234e37f421764f344f18b4a3a
Chris Lu <chris.lu@gmail.com>
2019-10-13 19:52:31 -0700

    fix volume create

--- weed/storage/volume_create_linux.go
+++ weed/storage/volume_create_linux.go
@@ -10,7 +10,7 @@ import (
 )
 
 func createVolumeFile(fileName string, preallocate int64) (file *os.File, e error) {
-	file, e = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
+	file, e = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if preallocate != 0 {
 		syscall.Fallocate(int(file.Fd()), 1, 0, preallocate)
 		glog.V(0).Infof("Preallocated %d bytes disk space for %s", preallocate, fileName)
