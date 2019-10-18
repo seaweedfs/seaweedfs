2c455841eac1edbe415e237b0c4778ea14b50482
j.laycock <joseph.laycock@4sightimaging.com>
2019-10-18 11:01:45 +0100

    Make volumeCreate more consistent between all 3 implementations.

--- weed/storage/volume_create_linux.go
+++ weed/storage/volume_create_linux.go
@@ -9,8 +9,8 @@ import (
 	"github.com/chrislusf/seaweedfs/weed/glog"
 )
 
-func createVolumeFile(fileName string, preallocate int64, useMemoryMap uint32) (file *os.File, e error) {
-	file, e = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+func createVolumeFile(fileName string, preallocate int64, memoryMapSizeMB uint32) (*os.File, error) {
+	file, e := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
 	if preallocate != 0 {
 		syscall.Fallocate(int(file.Fd()), 1, 0, preallocate)
 		glog.V(0).Infof("Preallocated %d bytes disk space for %s", preallocate, fileName)
