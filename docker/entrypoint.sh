06ff984786318cccb89034a84db1ff9dbf430747
Chris Lu <chris.lu@gmail.com>
2019-12-11 21:38:54 -0800

    set master ip in docker compose yaml file
    fix https://github.com/chrislusf/seaweedfs/issues/1118

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 case "$1" in
 
   'master')
-  	ARGS="-ip `hostname -i` -mdir /data"
+  	ARGS="-mdir /data"
 	# Is this instance linked with an other master? (Docker commandline "--link master1:master")
 	if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
 		ARGS="$ARGS -peers=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
