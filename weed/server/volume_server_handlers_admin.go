4ff513d64d1c9b94d7cb7cf342477f6bef19dd36
LazyDBA247-Anyvision <yonin@anyvision.co>
2020-02-23 23:27:09 +0200

    staus route: add DiskStatuses for disk in the volume server status
    whem monitoring server, better to know the status of the disks & volumes in a single route.

--- weed/server/volume_server_handlers_admin.go
+++ weed/server/volume_server_handlers_admin.go
@@ -12,6 +12,13 @@ import (
 func (vs *VolumeServer) statusHandler(w http.ResponseWriter, r *http.Request) {
 	m := make(map[string]interface{})
 	m["Version"] = util.VERSION
+	var ds []*volume_server_pb.DiskStatus
+	for _, loc := range vs.store.Locations {
+		if dir, e := filepath.Abs(loc.Directory); e == nil {
+			ds = append(ds, stats.NewDiskStatus(dir))
+		}
+	}
+	m["DiskStatuses"] = ds
 	m["Volumes"] = vs.store.VolumeInfos()
 	writeJsonQuiet(w, r, http.StatusOK, m)
 }
