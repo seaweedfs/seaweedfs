f7b5a1d6979835d97de1750896c0ebe366c5241c
Chris Lu <chris.lu@gmail.com>
2020-04-04 13:17:23 -0700

    add disk stat to logs

--- weed/stats/disk.go
+++ weed/stats/disk.go
@@ -1,9 +1,13 @@
 package stats
 
-import "github.com/chrislusf/seaweedfs/weed/pb/volume_server_pb"
+import (
+	"github.com/chrislusf/seaweedfs/weed/glog"
+	"github.com/chrislusf/seaweedfs/weed/pb/volume_server_pb"
+)
 
 func NewDiskStatus(path string) (disk *volume_server_pb.DiskStatus) {
 	disk = &volume_server_pb.DiskStatus{Dir: path}
 	fillInDiskStatus(disk)
+	glog.V(0).Infof("read disk size: %v", disk)
 	return
 }
