4642ed565e1b59e8cf929fc2e5d10efc32364f39
Chris Lu <chris.lu@gmail.com>
2020-04-19 10:42:58 -0700

    fix tests

--- weed/filer2/leveldb/leveldb_store_test.go
+++ weed/filer2/leveldb/leveldb_store_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestCreateAndFind(t *testing.T) {
-	filer := filer2.NewFiler(nil, nil, 0, "", "", nil)
+	filer := filer2.NewFiler(nil, nil, "",0, "", "", nil)
 	dir, _ := ioutil.TempDir("", "seaweedfs_filer_test")
 	defer os.RemoveAll(dir)
 	store := &LevelDBStore{}
@@ -66,7 +66,7 @@ func TestCreateAndFind(t *testing.T) {
 }
 
 func TestEmptyRoot(t *testing.T) {
-	filer := filer2.NewFiler(nil, nil, 0, "", "", nil)
+	filer := filer2.NewFiler(nil, nil, "", 0, "", "", nil)
 	dir, _ := ioutil.TempDir("", "seaweedfs_filer_test2")
 	defer os.RemoveAll(dir)
 	store := &LevelDBStore{}
