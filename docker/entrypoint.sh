232b0f0e6e6f12b05f0eb6e1a205bd99818fcbf0
Chris Lu <chris.lu@gmail.com>
2020-05-21 22:19:06 -0700

    allow overwrite flags

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,27 +4,27 @@ case "$1" in
 
   'master')
   	ARGS="-mdir=/data -volumePreallocate -volumeSizeLimitMB=1024"
-  	exec /usr/bin/weed $@ $ARGS
+  	exec /usr/bin/weed $ARGS $@
 	;;
 
   'volume')
   	ARGS="-dir=/data -max=0"
-  	exec /usr/bin/weed $@ $ARGS
+  	exec /usr/bin/weed $ARGS $@
 	;;
 
   'server')
   	ARGS="-dir=/data -volume.max=0 -master.volumePreallocate -master.volumeSizeLimitMB=1024"
-  	exec /usr/bin/weed $@ $ARGS
+  	exec /usr/bin/weed $ARGS $@
   	;;
 
   'filer')
   	ARGS=""
-  	exec /usr/bin/weed $@ $ARGS
+  	exec /usr/bin/weed $ARGS $@
 	;;
 
   's3')
   	ARGS="-domainName=$S3_DOMAIN_NAME -key.file=$S3_KEY_FILE -cert.file=$S3_CERT_FILE"
-  	exec /usr/bin/weed $@ $ARGS
+  	exec /usr/bin/weed $ARGS $@
 	;;
 
   'cronjob')
