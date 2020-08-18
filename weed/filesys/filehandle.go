618b2f6829af2bbf8cfed3a543ee13f675bd8efd
Chris Lu <chris.lu@gmail.com>
2020-08-18 12:53:08 -0700

    release resources only when needed to

--- weed/filesys/filehandle.go
+++ weed/filesys/filehandle.go
@@ -167,9 +167,9 @@ func (fh *FileHandle) Release(ctx context.Context, req *fuse.ReleaseRequest) err
 	if fh.f.isOpen <= 0 {
 		fh.dirtyPages.releaseResource()
 		fh.f.wfs.ReleaseHandle(fh.f.fullpath(), fuse.HandleID(fh.handle))
+		fh.f.entryViewCache = nil
+		fh.f.reader = nil
 	}
-	fh.f.entryViewCache = nil
-	fh.f.reader = nil
 
 	return nil
 }
