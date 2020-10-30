7ecbb4b3c9435ad75fee29245c6165485dacb892
Chris Lu <chris.lu@gmail.com>
2020-10-30 10:46:31 -0700

    clean up

--- weed/command/filer_copy.go
+++ weed/command/filer_copy.go
@@ -219,7 +219,7 @@ func genFileCopyTask(fileOrDir string, destPath string, fileCopyTaskChan chan Fi
 
 	fileCopyTaskChan <- FileCopyTask{
 		sourceLocation:     fileOrDir,
-		destinationUrlPath: destPath,//+fi.Name(),
+		destinationUrlPath: destPath,
 		fileSize:           fi.Size(),
 		fileMode:           fi.Mode(),
 		uid:                uid,
