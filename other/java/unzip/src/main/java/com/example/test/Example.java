7d249808407588431f00424dbb2ca599cc9441c6
Chris Lu <chris.lu@gmail.com>
2020-11-13 11:37:50 -0800

    HCFS: 1.5.4

--- other/java/unzip/src/main/java/com/example/test/Example.java
+++ other/java/unzip/src/main/java/com/example/test/Example.java
@@ -17,8 +17,13 @@ public class Example {
 
     public static void main(String[] args) throws IOException {
 
+        long startTime = System.currentTimeMillis();
         // 本地模式，速度很快
         parseZip("/Users/chris/tmp/test.zip");
+
+        long startTime2 = System.currentTimeMillis();
+
+        long localProcessTime = startTime2 - startTime;
 
         // swfs读取，慢
         SeaweedInputStream seaweedInputStream = new SeaweedInputStream(
@@ -28,6 +33,11 @@ public class Example {
                 filerClient.lookupEntry("/", "test.zip")
         );
         parseZip(seaweedInputStream);
+
+        long swProcessTime = System.currentTimeMillis() - startTime2;
+
+        System.out.println("Local time: " + localProcessTime);
+        System.out.println("SeaweedFS time: " + swProcessTime);
 
     }
 
