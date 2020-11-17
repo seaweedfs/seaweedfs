6204dc6e8524b1a17cb946c1157b5d741425c12f
Chris Lu <chris.lu@gmail.com>
2020-11-16 16:57:31 -0800

    add comment

--- weed/server/filer_server.go
+++ weed/server/filer_server.go
@@ -115,6 +115,7 @@ func NewFilerServer(defaultMux, readonlyMux *http.ServeMux, option *FilerOption)
 	v.SetDefault("filer.options.buckets_folder", "/buckets")
 	fs.filer.DirBucketsPath = v.GetString("filer.options.buckets_folder")
 	// TODO deprecated, will be be removed after 2020-12-31
+	// replaced by https://github.com/chrislusf/seaweedfs/wiki/Path-Specific-Configuration
 	fs.filer.FsyncBuckets = v.GetStringSlice("filer.options.buckets_fsync")
 	fs.filer.LoadConfiguration(v)
 
