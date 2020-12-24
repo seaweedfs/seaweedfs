a09cd28986077dc3fe9252475a4da16d8acaad98
Chris Lu <chris.lu@gmail.com>
2020-12-24 01:42:15 -0800

    add debug messages

--- weed/s3api/s3api_objects_list_handlers.go
+++ weed/s3api/s3api_objects_list_handlers.go
@@ -310,6 +310,8 @@ func getListObjectsV1Args(values url.Values) (prefix, marker, delimiter string, 
 
 func (s3a *S3ApiServer) isDirectoryAllEmpty(filerClient filer_pb.SeaweedFilerClient, parentDir, name string) (isEmpty bool, err error) {
 	// println("+ isDirectoryAllEmpty", dir, name)
+	glog.V(4).Infof("+ start parentDir: %s name: %s", parentDir, name)
+	defer glog.V(4).Infof("- start parentDir: %s name: %s isEmpty: %v", parentDir, name, isEmpty)
 	var fileCounter int
 	var subDirs []string
 	currentDir := parentDir + "/" + name
@@ -324,6 +326,7 @@ func (s3a *S3ApiServer) isDirectoryAllEmpty(filerClient filer_pb.SeaweedFilerCli
 			}
 			startFrom = entry.Name
 			isExhausted = isExhausted || isLast
+			glog.V(4).Infof("list: %s/%s isExhausted: %t", currentDir, startFrom, isExhausted)
 			return nil
 		}, startFrom, false, 8)
 	}
