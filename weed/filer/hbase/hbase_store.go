2fea8cfa0fd56d7f2f459681b727d9625b62bc16
Chris Lu <chris.lu@gmail.com>
2020-12-23 23:54:15 -0800

    fix compilation

--- weed/filer/hbase/hbase_store.go
+++ weed/filer/hbase/hbase_store.go
@@ -135,7 +135,7 @@ func (store *HbaseStore) DeleteFolderChildren(ctx context.Context, path util.Ful
 		}
 		fullpath := util.FullPath(cell.Row)
 		dir, _ := fullpath.DirAndName()
-		if dir != string(dir) {
+		if dir != string(path) {
 			continue
 		}
 
