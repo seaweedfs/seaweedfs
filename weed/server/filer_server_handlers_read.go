7458ff952311deff8aa3ca1a8ca13f788e311dce
henry <zhanggm@sugon.com>
2020-12-28 11:28:33 +0800

    Solve the Chinese name disorder of download file

--- weed/server/filer_server_handlers_read.go
+++ weed/server/filer_server_handlers_read.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"mime"
 	"net/http"
+	"net/url"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -121,6 +122,7 @@ func (fs *FilerServer) GetOrHeadHandler(w http.ResponseWriter, r *http.Request, 
 	setEtag(w, etag)
 
 	filename := entry.Name()
+	filename = url.QueryEscape(filename)
 	adjustHeaderContentDisposition(w, r, filename)
 
 	totalSize := int64(entry.Size())
