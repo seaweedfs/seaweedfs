ebb223c190385a92c72b4d8096e71558c9f03354
qieqieplus <admin@qieqie.me>
2021-01-04 21:21:46 +0800

    fix for rocksdb kv

--- weed/filer/rocksdb/rocksdb_store_kv.go
+++ weed/filer/rocksdb/rocksdb_store_kv.go
@@ -5,15 +5,13 @@ package rocksdb
 import (
 	"context"
 	"fmt"
-	"github.com/tecbot/gorocksdb"
 
 	"github.com/chrislusf/seaweedfs/weed/filer"
 )
 
 func (store *RocksDBStore) KvPut(ctx context.Context, key []byte, value []byte) (err error) {
 
-	wo := gorocksdb.NewDefaultWriteOptions()
-	err = store.db.Put(wo, key, value)
+	err = store.db.Put(store.wo, key, value)
 
 	if err != nil {
 		return fmt.Errorf("kv put: %v", err)
@@ -24,8 +22,7 @@ func (store *RocksDBStore) KvPut(ctx context.Context, key []byte, value []byte) 
 
 func (store *RocksDBStore) KvGet(ctx context.Context, key []byte) (value []byte, err error) {
 
-	ro := gorocksdb.NewDefaultReadOptions()
-	value, err = store.db.GetBytes(ro, key)
+	value, err = store.db.GetBytes(store.ro, key)
 
 	if value == nil {
 		return nil, filer.ErrKvNotFound
@@ -40,8 +37,7 @@ func (store *RocksDBStore) KvGet(ctx context.Context, key []byte) (value []byte,
 
 func (store *RocksDBStore) KvDelete(ctx context.Context, key []byte) (err error) {
 
-	wo := gorocksdb.NewDefaultWriteOptions()
-	err = store.db.Delete(wo, key)
+	err = store.db.Delete(store.wo, key)
 
 	if err != nil {
 		return fmt.Errorf("kv delete: %v", err)
