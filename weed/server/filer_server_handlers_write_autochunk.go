a801332b0dc44ea3239e437b845e210c840e125e
Chris Lu <chris.lu@gmail.com>
2021-03-24 01:03:11 -0700

    filer: return 409 if file conflicts with a directory
    fix https://github.com/chrislusf/seaweedfs/issues/1938

--- weed/server/filer_server_handlers_write_autochunk.go
+++ weed/server/filer_server_handlers_write_autochunk.go
@@ -59,6 +59,8 @@ func (fs *FilerServer) autoChunk(ctx context.Context, w http.ResponseWriter, r *
 	if err != nil {
 		if strings.HasPrefix(err.Error(), "read input:") {
 			writeJsonError(w, r, 499, err)
+		}else if strings.HasSuffix(err.Error(), "is a file") {
+			writeJsonError(w, r, http.StatusConflict, err)
 		} else {
 			writeJsonError(w, r, http.StatusInternalServerError, err)
 		}
