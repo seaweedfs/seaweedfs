11c120c040c82a2eecc6f7f5d23b83266e22eb5e
Chris Lu <chris.lu@gmail.com>
2021-04-22 14:22:46 -0700

    master UI adds volume size limit

--- weed/server/master_server_handlers_ui.go
+++ weed/server/master_server_handlers_ui.go
@@ -14,17 +14,19 @@ func (ms *MasterServer) uiStatusHandler(w http.ResponseWriter, r *http.Request) 
 	infos := make(map[string]interface{})
 	infos["Up Time"] = time.Now().Sub(startTime).String()
 	args := struct {
-		Version    string
-		Topology   interface{}
-		RaftServer raft.Server
-		Stats      map[string]interface{}
-		Counters   *stats.ServerStats
+		Version           string
+		Topology          interface{}
+		RaftServer        raft.Server
+		Stats             map[string]interface{}
+		Counters          *stats.ServerStats
+		VolumeSizeLimitMB uint
 	}{
 		util.Version(),
 		ms.Topo.ToMap(),
 		ms.Topo.RaftServer,
 		infos,
 		serverStats,
+		ms.option.VolumeSizeLimitMB,
 	}
 	ui.StatusTpl.Execute(w, args)
 }
