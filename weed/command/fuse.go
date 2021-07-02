586e066897c03fc4084d6ad0eaa8d77128e48f8c
danielflira <danielflira@gmail.com>
2021-07-02 16:52:52 -0300

    fix weed fuse parameters parsing

--- weed/command/fuse.go
+++ weed/command/fuse.go
@@ -55,14 +55,14 @@ func runFuse(cmd *Command, args []string) bool {
 			name := option.String()
 			option.Reset()
 
-			for i++; i < rawArgsLen && rawArgs[i] != ','; i++ {
+			for i++; i < rawArgsLen && rawArgs[i] != ',' && rawArgs[i] != ' '; i++ {
 				// double quote separator read option until next double quote
 				if rawArgs[i] == '"' {
 					for i++; i < rawArgsLen && rawArgs[i] != '"'; i++ {
 						option.WriteByte(rawArgs[i])
 					}
 
-					// single quote separator read option until next single quote
+				// single quote separator read option until next single quote
 				} else if rawArgs[i] == '\'' {
 					for i++; i < rawArgsLen && rawArgs[i] != '\''; i++ {
 						option.WriteByte(rawArgs[i])
