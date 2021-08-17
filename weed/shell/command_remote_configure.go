ccb3df41f84f2f484692654585e100cf54739c95
Chris Lu <chris.lu@gmail.com>
2021-08-17 11:27:08 -0700

    elide secret key

--- weed/shell/command_remote_configure.go
+++ weed/shell/command_remote_configure.go
@@ -95,7 +95,7 @@ func (c *commandRemoteConfigure) listExistingRemoteStorages(commandEnv *CommandE
 			return fmt.Errorf("unmarshal %s/%s: %v", filer.DirectoryEtcRemote, entry.Name, err)
 		}
 
-		conf.S3SecretKey = ""
+		conf.S3SecretKey = strings.Repeat("*", len(conf.S3SecretKey))
 
 		m := jsonpb.Marshaler{
 			EmitDefaults: false,
