2e9372dcf7567e41074eac0a6f3698dda931453d
Chris Lu <chris.lu@gmail.com>
2021-09-27 03:07:44 -0700

    volume stream read skips deleted content

--- weed/server/volume_grpc_read_all.go
+++ weed/server/volume_grpc_read_all.go
@@ -5,7 +5,6 @@ import (
 	"github.com/chrislusf/seaweedfs/weed/pb/volume_server_pb"
 	"github.com/chrislusf/seaweedfs/weed/storage"
 	"github.com/chrislusf/seaweedfs/weed/storage/needle"
-	"github.com/chrislusf/seaweedfs/weed/storage/super_block"
 )
 
 func (vs *VolumeServer) ReadAllNeedles(req *volume_server_pb.ReadAllNeedlesRequest, stream volume_server_pb.VolumeServer_ReadAllNeedlesServer) (err error) {
@@ -24,9 +23,9 @@ func (vs *VolumeServer) streaReadOneVolume(vid needle.VolumeId, stream volume_se
 		return fmt.Errorf("not found volume id %d", vid)
 	}
 
-	scanner := &VolumeFileScanner4ReadAll{
-		stream: stream,
-		v:      v,
+	scanner := &storage.VolumeFileScanner4ReadAll{
+		Stream: stream,
+		V:      v,
 	}
 
 	offset := int64(v.SuperBlock.BlockSize())
@@ -34,31 +33,4 @@ func (vs *VolumeServer) streaReadOneVolume(vid needle.VolumeId, stream volume_se
 	err = storage.ScanVolumeFileFrom(v.Version(), v.DataBackend, offset, scanner)
 
 	return err
-}
-
-type VolumeFileScanner4ReadAll struct {
-	stream volume_server_pb.VolumeServer_ReadAllNeedlesServer
-	v      *storage.Volume
-}
-
-func (scanner *VolumeFileScanner4ReadAll) VisitSuperBlock(superBlock super_block.SuperBlock) error {
-	return nil
-
-}
-func (scanner *VolumeFileScanner4ReadAll) ReadNeedleBody() bool {
-	return true
-}
-
-func (scanner *VolumeFileScanner4ReadAll) VisitNeedle(n *needle.Needle, offset int64, needleHeader, needleBody []byte) error {
-
-	sendErr := scanner.stream.Send(&volume_server_pb.ReadAllNeedlesResponse{
-		VolumeId:   uint32(scanner.v.Id),
-		NeedleId:   uint64(n.Id),
-		Cookie:     uint32(n.Cookie),
-		NeedleBlob: n.Data,
-	})
-	if sendErr != nil {
-		return sendErr
-	}
-	return nil
 }
