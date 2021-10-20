0b834600d5f8587990ce53c6c51b9508418bf44b
Konstantin Lebedev <lebedev_k@tochka.com>
2021-10-20 17:58:06 +0500

    fix PutObjectTaggingHandler Unmarshal

--- weed/s3api/tags.go
+++ weed/s3api/tags.go
@@ -14,7 +14,7 @@ type TagSet struct {
 }
 
 type Tagging struct {
-	XMLName xml.Name `xml:"http://s3.amazonaws.com/doc/2006-03-01/ Tagging"`
+	XMLName xml.Name `xml:"Tagging"`
 	TagSet  TagSet   `xml:"TagSet"`
 }
 
