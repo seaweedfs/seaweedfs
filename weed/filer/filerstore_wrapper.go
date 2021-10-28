900e888695c8058d1a97a2314b3dfabc6942f463
Chris Lu <chris.lu@gmail.com>
2021-10-27 23:45:48 -0700

    filer: avoid possible prefixed pagination loop on unsupported filer stores

--- weed/filer/filerstore_wrapper.go
+++ weed/filer/filerstore_wrapper.go
@@ -296,7 +296,7 @@ func (fsw *FilerStoreWrapper) prefixFilterEntries(ctx context.Context, dirPath u
 		}
 		if count < limit {
 			notPrefixed = notPrefixed[:0]
-			_, err = actualStore.ListDirectoryEntries(ctx, dirPath, lastFileName, false, limit, func(entry *Entry) bool {
+			lastFileName, err = actualStore.ListDirectoryEntries(ctx, dirPath, lastFileName, false, limit, func(entry *Entry) bool {
 				notPrefixed = append(notPrefixed, entry)
 				return true
 			})
