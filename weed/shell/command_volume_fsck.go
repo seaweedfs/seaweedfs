9ccfc1cfee63dda1261a73a82f6d077e206daa79
Chris Lu <chris.lu@gmail.com>
2021-11-28 22:39:24 -0800

    Update command_volume_fsck.go
    minor

--- weed/shell/command_volume_fsck.go
+++ weed/shell/command_volume_fsck.go
@@ -216,11 +216,11 @@ func (c *commandVolumeFsck) findExtraChunksInVolumeServers(volumeIdToVInfo map[u
 			}
 			if inUseCount == 0 {
 				if err := deleteVolume(c.env.option.GrpcDialOption, needle.VolumeId(volumeId), vinfo.server); err != nil {
-					return fmt.Errorf("delete volume %d: %v\n", volumeId, err)
+					return fmt.Errorf("delete volume %d: %v", volumeId, err)
 				}
 			} else {
 				if err := c.purgeFileIdsForOneVolume(volumeId, orphanFileIds, writer); err != nil {
-					return fmt.Errorf("purge for volume %d: %v\n", volumeId, err)
+					return fmt.Errorf("purge for volume %d: %v", volumeId, err)
 				}
 			}
 		}
