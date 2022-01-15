1bd6d289d48ea3ae3c1461bf090ce0ffa6f2505f
chrislu <chris.lu@gmail.com>
2022-01-15 05:45:29 -0800

    better locking on file handle

--- weed/filesys/filehandle.go
+++ weed/filesys/filehandle.go
@@ -26,7 +26,6 @@ type FileHandle struct {
 	contentType    string
 	handle         uint64
 	sync.Mutex
-	sync.WaitGroup
 
 	f         *File
 	RequestId fuse.RequestID // unique ID for request
@@ -62,9 +61,6 @@ var _ = fs.HandleWriter(&FileHandle{})
 var _ = fs.HandleReleaser(&FileHandle{})
 
 func (fh *FileHandle) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
-
-	fh.Add(1)
-	defer fh.Done()
 
 	fh.Lock()
 	defer fh.Unlock()
@@ -173,9 +169,6 @@ func (fh *FileHandle) readFromChunks(buff []byte, offset int64) (int64, error) {
 // Write to the file handle
 func (fh *FileHandle) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
 
-	fh.Add(1)
-	defer fh.Done()
-
 	fh.dirtyPages.writerPattern.MonitorWriteAt(req.Offset, len(req.Data))
 
 	fh.Lock()
@@ -217,8 +210,6 @@ func (fh *FileHandle) Release(ctx context.Context, req *fuse.ReleaseRequest) err
 
 	glog.V(4).Infof("Release %v fh %d open=%d", fh.f.fullpath(), fh.handle, fh.f.isOpen)
 
-	fh.Wait()
-
 	fh.f.wfs.handlesLock.Lock()
 	fh.f.isOpen--
 	fh.f.wfs.handlesLock.Unlock()
@@ -249,9 +240,6 @@ func (fh *FileHandle) Flush(ctx context.Context, req *fuse.FlushRequest) error {
 		glog.V(4).Infof("Flush %v fh %d skip deleted", fh.f.fullpath(), fh.handle)
 		return nil
 	}
-
-	fh.Add(1)
-	defer fh.Done()
 
 	fh.Lock()
 	defer fh.Unlock()
