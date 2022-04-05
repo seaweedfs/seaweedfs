1ae7f509f5724deb782e87a02001ec4a7884bc6e
Konstantin Lebedev <9497591+kmlebedev@users.noreply.github.com>
2022-04-05 13:50:39 +0500

    LocalID from raw string

--- weed/server/raft_hashicorp.go
+++ weed/server/raft_hashicorp.go
@@ -73,7 +73,7 @@ func NewHashicorpRaftServer(option *RaftServerOption) (*RaftServer, error) {
 	}
 
 	c := raft.DefaultConfig()
-	c.LocalID = raft.ServerID(s.serverAddr.String()) // TODO maybee the IP:port address will change
+	c.LocalID = raft.ServerID(s.serverAddr) // TODO maybee the IP:port address will change
 	c.NoSnapshotRestoreOnStart = option.RaftResumeState
 	c.HeartbeatTimeout = time.Duration(float64(option.HeartbeatInterval) * (rand.Float64()*0.25 + 1))
 	c.ElectionTimeout = option.ElectionTimeout
