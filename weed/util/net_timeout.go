ef6c6c450e1e062de1b0197ff0e1f83060c5f3b1
chrislu <chris.lu@gmail.com>
2022-05-04 14:55:14 -0700

    avoid fatal error if port is already in use

--- weed/util/net_timeout.go
+++ weed/util/net_timeout.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"github.com/chrislusf/seaweedfs/weed/glog"
 	"net"
 	"time"
 
@@ -112,7 +113,8 @@ func NewIpAndLocalListeners(host string, port int, timeout time.Duration) (ipLis
 	if host != "localhost" && host != "" && host != "0.0.0.0" && host != "127.0.0.1" && host != "[::]" && host != "[::1]" {
 		listner, err = net.Listen("tcp", JoinHostPort("localhost", port))
 		if err != nil {
-			return
+			glog.V(0).Infof("skip starting on %s:%d: %v", host, port, err)
+			return ipListner, nil, nil
 		}
 
 		localListener = &Listener{
