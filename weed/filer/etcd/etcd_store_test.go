db33b0373532fbd34472bda12e91910634b46fc4
chrislu <chris.lu@gmail.com>
2022-05-06 02:19:47 -0700

    add comments

--- weed/filer/etcd/etcd_store_test.go
+++ weed/filer/etcd/etcd_store_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestStore(t *testing.T) {
-	if false {
+	if false { // turn on the testing when local env is set up
 		store := &EtcdStore{}
 		store.initialize("localhost:2379", "3s")
 		store_test.TestFilerStore(t, store)
