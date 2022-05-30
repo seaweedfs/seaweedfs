af562e1a208b973e187a60a20629d7bfde8809fb
Konstantin Lebedev <9497591+kmlebedev@users.noreply.github.com>
2022-05-30 23:26:26 +0500

    rm log info

--- weed/s3api/s3api_object_handlers.go
+++ weed/s3api/s3api_object_handlers.go
@@ -94,7 +94,6 @@ func (s3a *S3ApiServer) PutObjectHandler(w http.ResponseWriter, r *http.Request)
 
 	if strings.HasSuffix(object, "/") {
 		if err := s3a.mkdir(s3a.option.BucketsPath, bucket+strings.TrimSuffix(object, "/"), func(entry *filer_pb.Entry) {
-			glog.V(0).Infof("make file %s", bucket+object)
 			entry.Attributes.Mime = r.Header.Get("Content-Type")
 		}); err != nil {
 			s3err.WriteErrorResponse(w, r, s3err.ErrInternalError)
