62f74f5e3cce0668478c179f81fd1a9c5916e455
zzq09494 <zzq09494@ly.com>
2022-06-16 13:21:25 +0800

    add bucket label to s3 prometheus metrics

--- weed/s3api/stats.go
+++ weed/s3api/stats.go
@@ -1,6 +1,7 @@
 package s3api
 
 import (
+	"github.com/chrislusf/seaweedfs/weed/s3api/s3_constants"
 	stats_collect "github.com/chrislusf/seaweedfs/weed/stats"
 	"net/http"
 	"strconv"
@@ -27,11 +28,12 @@ func (r *StatusRecorder) Flush() {
 
 func track(f http.HandlerFunc, action string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		bucket, _ := s3_constants.GetBucketAndObject(r)
 		w.Header().Set("Server", "SeaweedFS S3")
 		recorder := NewStatusResponseWriter(w)
 		start := time.Now()
 		f(recorder, r)
-		stats_collect.S3RequestHistogram.WithLabelValues(action).Observe(time.Since(start).Seconds())
-		stats_collect.S3RequestCounter.WithLabelValues(action, strconv.Itoa(recorder.Status)).Inc()
+		stats_collect.S3RequestHistogram.WithLabelValues(action, bucket).Observe(time.Since(start).Seconds())
+		stats_collect.S3RequestCounter.WithLabelValues(action, strconv.Itoa(recorder.Status), bucket).Inc()
 	}
 }
