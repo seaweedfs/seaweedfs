fa4d0093e17f710c3da00545022646cb96a6fd98
chrislu <chris.lu@gmail.com>
2022-08-15 16:48:23 -0700

    3.22

--- weed/util/constants.go
+++ weed/util/constants.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	VERSION_NUMBER = fmt.Sprintf("%.02f", 3.21)
+	VERSION_NUMBER = fmt.Sprintf("%.02f", 3.22)
 	VERSION        = sizeLimit + " " + VERSION_NUMBER
 	COMMIT         = ""
 )
