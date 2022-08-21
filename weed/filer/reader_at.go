3f758820c1f93540b77bfa6a13e840d8e9f75e1a
Patrick Schmidt <patrick.schmidt@innogames.com>
2022-08-21 20:33:58 +0200

    Fix FUSE server buffer leaks in file gaps (#3472)
    * Fix FUSE server buffer leaks in file gaps
    
    This change zeros read buffers when encountering file gaps during
    file/chunk reads in FUSE mounts.
    It prevents leaking internal buffers of the FUSE server which could
    otherwise reveal metadata, directory listings, file contents and
    other data related to FUSE API calls.
    The issue was that buffers are reused, but when a file gap was found
    the buffer was not zeroed accordingly and the existing data of the
    buffer was kept and returned.
    
    * Move zero logic into its own method

--- weed/filer/reader_at.go
+++ weed/filer/reader_at.go
@@ -127,10 +127,11 @@ func (c *ChunkReadAt) doReadAt(p []byte, offset int64) (n int, err error) {
 			nextChunks = c.chunkViews[i+1:]
 		}
 		if startOffset < chunk.LogicOffset {
-			gap := int(chunk.LogicOffset - startOffset)
+			gap := chunk.LogicOffset - startOffset
 			glog.V(4).Infof("zero [%d,%d)", startOffset, chunk.LogicOffset)
-			n += int(min(int64(gap), remaining))
-			startOffset, remaining = chunk.LogicOffset, remaining-int64(gap)
+			c.zero(p, startOffset-offset, gap)
+			n += int(min(gap, remaining))
+			startOffset, remaining = chunk.LogicOffset, remaining-gap
 			if remaining <= 0 {
 				break
 			}
@@ -154,10 +155,19 @@ func (c *ChunkReadAt) doReadAt(p []byte, offset int64) (n int, err error) {
 
 	// glog.V(4).Infof("doReadAt [%d,%d), n:%v, err:%v", offset, offset+int64(len(p)), n, err)
 
-	if err == nil && remaining > 0 && c.fileSize > startOffset {
-		delta := int(min(remaining, c.fileSize-startOffset))
-		glog.V(4).Infof("zero2 [%d,%d) of file size %d bytes", startOffset, startOffset+int64(delta), c.fileSize)
-		n += delta
+	// zero the remaining bytes if a gap exists at the end of the last chunk (or a fully sparse file)
+	if err == nil && remaining > 0 {
+		var delta int64
+		if c.fileSize > startOffset {
+			delta = min(remaining, c.fileSize-startOffset)
+			startOffset -= offset
+		} else {
+			delta = remaining
+			startOffset = max(startOffset-offset, startOffset-remaining-offset)
+		}
+		glog.V(4).Infof("zero2 [%d,%d) of file size %d bytes", startOffset, startOffset+delta, c.fileSize)
+		c.zero(p, startOffset, delta)
+		n += int(delta)
 	}
 
 	if err == nil && offset+int64(len(p)) >= c.fileSize {
@@ -194,4 +204,13 @@ func (c *ChunkReadAt) readChunkSliceAt(buffer []byte, chunkView *ChunkView, next
 	}
 	c.lastChunkFid = chunkView.FileId
 	return
+}
+
+func (c *ChunkReadAt) zero(buffer []byte, start, length int64) {
+	end := min(start+length, int64(len(buffer)))
+
+	// zero the bytes
+	for o := start; o < end; o++ {
+		buffer[o] = 0
+	}
 }
