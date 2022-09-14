990be6e3f9e1c090807b5d5b5a04af9b3e27eaee
Ryan Russell <git@ryanrussell.org>
2022-09-14 14:34:21 -0500

    refactor(command_ec_rebuild): `rebultErr` -> `rebuildErr`
    Signed-off-by: Ryan Russell <git@ryanrussell.org>

--- weed/shell/command_ec_rebuild.go
+++ weed/shell/command_ec_rebuild.go
@@ -178,14 +178,14 @@ func rebuildOneEcVolume(commandEnv *CommandEnv, rebuilder *EcNode, collection st
 func generateMissingShards(grpcDialOption grpc.DialOption, collection string, volumeId needle.VolumeId, sourceLocation pb.ServerAddress) (rebuiltShardIds []uint32, err error) {
 
 	err = operation.WithVolumeServerClient(false, sourceLocation, grpcDialOption, func(volumeServerClient volume_server_pb.VolumeServerClient) error {
-		resp, rebultErr := volumeServerClient.VolumeEcShardsRebuild(context.Background(), &volume_server_pb.VolumeEcShardsRebuildRequest{
+		resp, rebuildErr := volumeServerClient.VolumeEcShardsRebuild(context.Background(), &volume_server_pb.VolumeEcShardsRebuildRequest{
 			VolumeId:   uint32(volumeId),
 			Collection: collection,
 		})
-		if rebultErr == nil {
+		if rebuildErr == nil {
 			rebuiltShardIds = resp.RebuiltShardIds
 		}
-		return rebultErr
+		return rebuildErr
 	})
 	return
 }
