9df99ba0cc42830c25a8a086e965fdd12fa53430
changlin.shi <changlin.shi@ly.com>
2022-09-29 12:24:28 +0800

    s3: add acl headers

--- weed/s3api/s3_constants/header.go
+++ weed/s3api/s3_constants/header.go
@@ -37,12 +37,21 @@ const (
 	AmzObjectTaggingDirective = "X-Amz-Tagging-Directive"
 	AmzTagCount               = "x-amz-tagging-count"
 
+	// S3 ACL headers
+	AmzCannedAcl      = "X-Amz-Acl"
+	AmzAclFullControl = "X-Amz-Grant-Full-Control"
+	AmzAclRead        = "X-Amz-Grant-Read"
+	AmzAclWrite       = "X-Amz-Grant-Write"
+	AmzAclReadAcp     = "X-Amz-Grant-Read-Acp"
+	AmzAclWriteAcp    = "X-Amz-Grant-Write-Acp"
+
 	X_SeaweedFS_Header_Directory_Key = "x-seaweedfs-is-directory-key"
 )
 
 // Non-Standard S3 HTTP request constants
 const (
 	AmzIdentityId = "s3-identity-id"
+	AmzAccountId  = "s3-account-id"
 	AmzAuthType   = "s3-auth-type"
 	AmzIsAdmin    = "s3-is-admin" // only set to http request header as a context
 )
