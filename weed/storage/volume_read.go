56c94cc08ec8d5dfbc9aace0c1931334ef79f4b4
Eric Yang <lanqingy@usc.edu>
2022-09-29 12:48:36 -0700

    ADHOC: filter deleted files from idx file binary search (#3763)
    * ADHOC: filter deleted files from idx file binary search
    
    * remove unwanted check
    
    Co-authored-by: root <root@HQ-10MSTD3EY.roblox.local>

--- weed/storage/volume_read.go
+++ weed/storage/volume_read.go
@@ -84,10 +84,6 @@ func (v *Volume) readNeedle(n *needle.Needle, readOption *ReadOption, onReadSize
 func (v *Volume) readNeedleMetaAt(n *needle.Needle, offset int64, size int32) (err error) {
 	v.dataFileAccessLock.RLock()
 	defer v.dataFileAccessLock.RUnlock()
-	// read deleted meta data
-	if size < 0 {
-		size = -size
-	}
 	err = n.ReadNeedleMeta(v.DataBackend, offset, Size(size), v.Version())
 	if err == needle.ErrorSizeMismatch && OffsetSize == 4 {
 		err = n.ReadNeedleMeta(v.DataBackend, offset+int64(MaxPossibleVolumeSize), Size(size), v.Version())
