6146be497e2684edc226274177d44a066edab538
kvtb <76634406+kvtb@users.noreply.github.com>
2022-09-29 14:04:30 +0000

    Update SeaweedWrite.java

--- other/java/client/src/main/java/seaweedfs/client/SeaweedWrite.java
+++ other/java/client/src/main/java/seaweedfs/client/SeaweedWrite.java
@@ -149,7 +149,7 @@ public class SeaweedWrite {
         CloseableHttpResponse response = SeaweedUtil.getClosableHttpClient().execute(post);
 
         try {
-            if (response.getStatusLine().getStatusCode() != 200) {
+            if (response.getStatusLine().getStatusCode() / 100 != 2) {
                 if (response.getEntity().getContentType() != null && response.getEntity().getContentType().getValue().equals("application/json")) {
                     throw new IOException(EntityUtils.toString(response.getEntity(), "UTF-8"));
                 } else {
