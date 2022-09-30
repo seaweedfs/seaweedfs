c3487d3da7ad97cd21b7ab7e5109ecd5d46a01cb
chrislu <chris.lu@gmail.com>
2022-09-30 08:49:38 -0700

    better fix for option format

--- weed/command/fuse.go
+++ weed/command/fuse.go
@@ -191,7 +191,11 @@ func runFuse(cmd *Command, args []string) bool {
 		case "fusermount.path":
 			fusermountPath = parameter.value
 		default:
-			mountOptions.extraOptions = append(mountOptions.extraOptions, fmt.Sprintf("%s=%s", parameter.name, parameter.value))
+			t := parameter.name
+			if parameter.value != "true" {
+				t = fmt.Sprintf("%s=%s", parameter.name, parameter.value)
+			}
+			mountOptions.extraOptions = append(mountOptions.extraOptions, t)
 		}
 	}
 
