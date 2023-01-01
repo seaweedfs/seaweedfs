033a3d51b96b0a2d1df7bfe23cc5aa5a7bb058cf
chrislu <chris.lu@gmail.com>
2023-01-01 02:03:17 -0800

    add IntervalList for generic interval list

---
+++ weed/filer/interval_list.go
@@ -0,0 +1,128 @@
+package filer
+
+import "math"
+
+type Interval[T any] struct {
+	StartOffset int64
+	StopOffset  int64
+	TsNs        int64
+	Value       T
+	Prev        *Interval[T]
+	Next        *Interval[T]
+}
+
+func (interval *Interval[T]) Size() int64 {
+	return interval.StopOffset - interval.StartOffset
+}
+
+// IntervalList mark written intervals within one page chunk
+type IntervalList[T any] struct {
+	head *Interval[T]
+	tail *Interval[T]
+}
+
+func NewIntervalList[T any]() *IntervalList[T] {
+	list := &IntervalList[T]{
+		head: &Interval[T]{
+			StartOffset: -1,
+			StopOffset:  -1,
+		},
+		tail: &Interval[T]{
+			StartOffset: math.MaxInt64,
+			StopOffset:  math.MaxInt64,
+		},
+	}
+	list.head.Next = list.tail
+	list.tail.Prev = list.head
+	return list
+}
+
+func (list *IntervalList[T]) Front() (interval *Interval[T]) {
+	return list.head.Next
+}
+
+func (interval *Interval[T]) IsTail() bool {
+	return interval.Next == nil
+}
+
+func (list *IntervalList[T]) AppendInterval(interval *Interval[T]) {
+	interval.Next = list.tail
+	interval.Prev = list.tail.Prev
+	list.tail.Prev.Next = interval
+	list.tail.Prev = interval
+}
+
+func (list *IntervalList[T]) Overlay(startOffset, stopOffset, tsNs int64, value T) {
+	if startOffset >= stopOffset {
+		return
+	}
+	interval := &Interval[T]{
+		StartOffset: startOffset,
+		StopOffset:  stopOffset,
+		TsNs:        tsNs,
+		Value:       value,
+	}
+	list.addInterval(interval)
+}
+
+func (list *IntervalList[T]) addInterval(interval *Interval[T]) {
+
+	//t := list.head
+	//for ; t.Next != nil; t = t.Next {
+	//	if t.TsNs > interval.TsNs {
+	//		println("writes is out of order", t.TsNs-interval.TsNs, "ns")
+	//	}
+	//}
+
+	p := list.head
+	for ; p.Next != nil && p.Next.StopOffset <= interval.StartOffset; p = p.Next {
+	}
+	q := list.tail
+	for ; q.Prev != nil && q.Prev.StartOffset >= interval.StopOffset; q = q.Prev {
+	}
+
+	// left side
+	// interval after p.Next start
+	if p.Next.StartOffset < interval.StartOffset {
+		t := &Interval[T]{
+			StartOffset: p.Next.StartOffset,
+			StopOffset:  interval.StartOffset,
+			TsNs:        p.Next.TsNs,
+			Value:       p.Next.Value,
+		}
+		p.Next = t
+		t.Prev = p
+		t.Next = interval
+		interval.Prev = t
+	} else {
+		p.Next = interval
+		interval.Prev = p
+	}
+
+	// right side
+	// interval ends before p.Prev
+	if interval.StopOffset < q.Prev.StopOffset {
+		t := &Interval[T]{
+			StartOffset: interval.StopOffset,
+			StopOffset:  q.Prev.StopOffset,
+			TsNs:        q.Prev.TsNs,
+			Value:       q.Prev.Value,
+		}
+		q.Prev = t
+		t.Next = q
+		interval.Next = t
+		t.Prev = interval
+	} else {
+		q.Prev = interval
+		interval.Next = q
+	}
+
+}
+
+func (list *IntervalList[T]) Len() int {
+	var count int
+	for t := list.head; t != nil; t = t.Next {
+		count++
+	}
+	return count - 2
+}
---
+++ weed/filer/interval_list_test.go
@@ -0,0 +1,108 @@
+package filer
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestIntervalList_Overlay(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(0, 100, 1, 1)
+	list.Overlay(50, 150, 2, 2)
+	list.Overlay(200, 250, 3, 3)
+	list.Overlay(225, 250, 4, 4)
+	list.Overlay(175, 210, 5, 5)
+	list.Overlay(0, 25, 6, 6)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+	assert.Equal(t, 6, list.Len())
+	println()
+	list.Overlay(50, 150, 7, 7)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+	assert.Equal(t, 6, list.Len())
+}
+
+func TestIntervalList_Overlay2(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(0, 50, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay3(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(0, 60, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay4(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(0, 100, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay5(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(0, 110, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay6(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(50, 110, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay7(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(50, 90, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay8(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(60, 90, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay9(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(60, 100, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
+
+func TestIntervalList_Overlay10(t *testing.T) {
+	list := NewIntervalList[int]()
+	list.Overlay(50, 100, 1, 1)
+	list.Overlay(60, 110, 2, 2)
+	for p := list.Front(); !p.IsTail(); p = p.Next {
+		fmt.Printf("[%d,%d) %d %d\n", p.StartOffset, p.StopOffset, p.TsNs, p.Value)
+	}
+}
