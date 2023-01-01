c7c9d22f37de989b20fd06aaf9c6ea1411028834
CommanderRoot <CommanderRoot@users.noreply.github.com>
2023-01-01 14:07:53 +0100

    filer.store.mysql: Use utf8mb4 instead of 3 byte UTF8 (#4094)

--- docker/seaweedfs.sql
+++ docker/seaweedfs.sql
@@ -3,10 +3,10 @@ CREATE USER IF NOT EXISTS 'seaweedfs'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON seaweedfs.* TO 'seaweedfs'@'%';
 FLUSH PRIVILEGES;
 USE seaweedfs;
-CREATE TABLE IF NOT EXISTS filemeta (
-    dirhash     BIGINT         COMMENT 'first 64 bits of MD5 hash value of directory field',
-    name        VARCHAR(1000)  COMMENT 'directory or file name',
-    directory   TEXT           COMMENT 'full path to parent directory',
-    meta        LONGBLOB,
-    PRIMARY KEY (dirhash, name)
-) DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `filemeta` (
+    `dirhash`   BIGINT NOT NULL       COMMENT 'first 64 bits of MD5 hash value of directory field',
+    `name`      VARCHAR(766) NOT NULL COMMENT 'directory or file name',
+    `directory` TEXT NOT NULL         COMMENT 'full path to parent directory',
+    `meta`      LONGBLOB,
+    PRIMARY KEY (`dirhash`, `name`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
