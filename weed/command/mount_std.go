c5d737eae4659defcd8c2d93671f1ee7e3818200
zemul <zhouzemiao@ihuman.com>
2023-01-03 14:57:16 +0800

    add mount log

--- weed/command/mount_std.go
+++ weed/command/mount_std.go
@@ -256,7 +256,8 @@ func RunMount(option *MountOptions, umask os.FileMode) bool {
 
 	seaweedFileSystem.StartBackgroundTasks()
 
-	fmt.Printf("This is SeaweedFS version %s %s %s\n", util.Version(), runtime.GOOS, runtime.GOARCH)
+	glog.V(0).Infof("mounted %s%s to %v", *option.filer, mountRoot, dir)
+	glog.V(0).Infof("This is SeaweedFS version %s %s %s\n", util.Version(), runtime.GOOS, runtime.GOARCH)
 
 	server.Serve()
 
