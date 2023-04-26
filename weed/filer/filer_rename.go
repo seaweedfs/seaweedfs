5aed5fa3f6ab49df4da012ecdf57a0d42fc96fe1
wang wusong <wangwusong@virtaitech.com>
2023-04-26 20:51:12 +0800

    Fix: http rename move dir to subdir

--- weed/filer/filer_rename.go
+++ weed/filer/filer_rename.go
@@ -2,16 +2,23 @@ package filer
 
 import (
 	"fmt"
-	"github.com/seaweedfs/seaweedfs/weed/util"
 	"strings"
+
+	"github.com/seaweedfs/seaweedfs/weed/util"
 )
 
-func (f *Filer) CanRename(source, target util.FullPath) error {
+func (f *Filer) CanRename(source, target util.FullPath, oldName string) error {
+	sourcePath := source.Child(oldName)
+	if strings.HasPrefix(string(target), string(sourcePath)) {
+		return fmt.Errorf("mv: can not move directory to a subdirectory of itself")
+	}
+
 	sourceBucket := f.DetectBucket(source)
 	targetBucket := f.DetectBucket(target)
 	if sourceBucket != targetBucket {
 		return fmt.Errorf("can not move across collection %s => %s", sourceBucket, targetBucket)
 	}
+
 	return nil
 }
 
