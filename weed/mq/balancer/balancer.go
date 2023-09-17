4e17d399a6978af7dbbb6ba84e06827ad4add7f2
chrislu <chris.lu@gmail.com>
2023-09-17 00:04:20 -0700

    refactor

--- weed/mq/balancer/balancer.go
+++ weed/mq/balancer/balancer.go
@@ -14,29 +14,6 @@ type Balancer struct {
 	Brokers cmap.ConcurrentMap[string, *BrokerStats]
 }
 
-func (b *Balancer) LookupOrAllocateTopicPartitions(topic *mq_pb.Topic, publish bool) ([]*mq_pb.BrokerPartitionAssignment, error) {
-	// TODO lock the topic
-
-	// find the topic partitions on the filer
-	// if the topic is not found
-	//   if the request is_for_publish
-	//     create the topic
-	//   if the request is_for_subscribe
-	//     return error not found
-	// t := topic.FromPbTopic(request.Topic)
-	return []*mq_pb.BrokerPartitionAssignment{
-		{
-			LeaderBroker:    "localhost:17777",
-			FollowerBrokers: []string{"localhost:17777"},
-			Partition: &mq_pb.Partition{
-				RingSize:   MaxPartitionCount,
-				RangeStart: 0,
-				RangeStop:  MaxPartitionCount,
-			},
-		},
-	}, nil
-}
-
 type BrokerStats struct {
 	TopicPartitionCount int32
 	ConsumerCount       int32
