63c91ef289eb6f048e1f1273931c69b7b29c2fbd
Konstantin Lebedev <9497591+kmlebedev@users.noreply.github.co>
2023-09-21 23:23:57 +0500

    Replace action read/write to readAcp/writeAcp for handlers with acl query  https://github.com/seaweedfs/seaweedfs/issues/4519

--- weed/s3api/s3_constants/s3_actions.go
+++ weed/s3api/s3_constants/s3_actions.go
@@ -1,11 +1,13 @@
 package s3_constants
 
 const (
-	ACTION_READ    = "Read"
-	ACTION_WRITE   = "Write"
-	ACTION_ADMIN   = "Admin"
-	ACTION_TAGGING = "Tagging"
-	ACTION_LIST    = "List"
+	ACTION_READ      = "Read"
+	ACTION_READ_ACP  = "ReadAcp"
+	ACTION_WRITE     = "Write"
+	ACTION_WRITE_ACP = "WriteAcp"
+	ACTION_ADMIN     = "Admin"
+	ACTION_TAGGING   = "Tagging"
+	ACTION_LIST      = "List"
 
 	SeaweedStorageDestinationHeader = "x-seaweedfs-destination"
 	MultipartUploadsFolder          = ".uploads"
