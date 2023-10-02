567d78892860f87c207beb56a3323b4b9e90dfc9
chrislu <chris.lu@gmail.com>
2023-10-02 11:49:54 -0700

    Revert "Revert "Add disk type to prometheus metrics" (#4777)"
    This reverts commit 9215ba24be0aec0c5804927cc73613560e57cbc0.

--- weed/stats/metrics.go
+++ weed/stats/metrics.go
@@ -176,7 +176,7 @@ var (
 			Subsystem: "volumeServer",
 			Name:      "volumes",
 			Help:      "Number of volumes or shards.",
-		}, []string{"collection", "type"})
+		}, []string{"collection", "type", "disk"})
 
 	VolumeServerReadOnlyVolumeGauge = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
@@ -315,11 +315,10 @@ func SourceName(port uint32) string {
 	return net.JoinHostPort(hostname, strconv.Itoa(int(port)))
 }
 
-// todo - can be changed to DeletePartialMatch when https://github.com/prometheus/client_golang/pull/1013 gets released
 func DeleteCollectionMetrics(collection string) {
 	VolumeServerDiskSizeGauge.DeleteLabelValues(collection, "normal")
 	for _, volume_type := range readOnlyVolumeTypes {
-		VolumeServerReadOnlyVolumeGauge.DeleteLabelValues(collection, volume_type)
+		VolumeServerReadOnlyVolumeGauge.DeletePartialMatch(prometheus.Labels{"collection": collection, "type": volume_type})
 	}
-	VolumeServerVolumeCounter.DeleteLabelValues(collection, "volume")
+	VolumeServerVolumeCounter.DeletePartialMatch(prometheus.Labels{"collection": collection, "type": "volume"})
 }
