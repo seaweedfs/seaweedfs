8815e72765a93533dfd7becea65a0c172ab38a13
James Hedley <jameskhedley@gmail.com>
2023-10-13 15:31:52 +0100

    Update weed/command/s3.go
    Co-authored-by: Chris Lu <chrislusf@users.noreply.github.com>

--- weed/command/s3.go
+++ weed/command/s3.go
@@ -70,7 +70,7 @@ func init() {
 	s3StandaloneOptions.tlsPrivateKey = cmdS3.Flag.String("key.file", "", "path to the TLS private key file")
 	s3StandaloneOptions.tlsCertificate = cmdS3.Flag.String("cert.file", "", "path to the TLS certificate file")
 	s3StandaloneOptions.tlsCACertificate = cmdS3.Flag.String("cacert.file", "", "path to the TLS CA certificate file")
-	s3StandaloneOptions.verifyClientCert = cmdS3.Flag.Bool("Cert", false, "whether to verify the client's certificate")
+	s3StandaloneOptions.verifyClientCert = cmdS3.Flag.Bool("verifyClientCert", false, "whether to verify the client's certificate")
 	s3StandaloneOptions.metricsHttpPort = cmdS3.Flag.Int("metricsPort", 0, "Prometheus metrics listen port")
 	s3StandaloneOptions.allowEmptyFolder = cmdS3.Flag.Bool("allowEmptyFolder", true, "allow empty folders")
 	s3StandaloneOptions.allowDeleteBucketNotEmpty = cmdS3.Flag.Bool("allowDeleteBucketNotEmpty", true, "allow recursive deleting all entries along with bucket")
