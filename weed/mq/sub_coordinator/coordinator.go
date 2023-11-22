0b85d5dc350ad090625b1c95fd7fabc861b2f43d
chrislu <chris.lu@gmail.com>
2023-11-22 11:24:58 -0800

    rename

--- weed/mq/sub_coordinator/coordinator.go
+++ weed/mq/sub_coordinator/coordinator.go
@@ -6,14 +6,14 @@ import (
 )
 
 type ConsumerGroupInstance struct {
-	ClientId string
+	InstanceId string
 	// the consumer group instance may not have an active partition
 	Partition *topic.Partition
 	// processed message count
 	ProcessedMessageCount int64
 }
 type ConsumerGroup struct {
-	// map a client id to a consumer group instance
+	// map a consumer group instance id to a consumer group instance
 	ConsumerGroupInstances cmap.ConcurrentMap[string, *ConsumerGroupInstance]
 	MinimumActiveInstances int32
 	MaximumActiveInstances int32
@@ -26,11 +26,15 @@ type TopicConsumerGroups struct {
 // Coordinator coordinates the instances in the consumer group for one topic.
 // It is responsible for:
 // 1. Assigning partitions to consumer instances.
-// 2. Reassigning partitions when a consumer instance is down.
-// 3. Reassigning partitions when a consumer instance is up.
+// 2. Reassigning partitions when a consumer instance is up/down.
+// The coordinator manages for each consumer:
+// 1. The consumer group
+// 2. The consumer group instance
+// 3. The partitions that the consumer is consuming
+
 type Coordinator struct {
 	// map client id to subscriber
 	Subscribers cmap.ConcurrentMap[string, *ConsumerGroupInstance]
 	// map topic name to consumer groups
-	TopicSubscribers cmap.ConcurrentMap[string, map[string]TopicConsumerGroups]
+	TopicSubscribers cmap.ConcurrentMap[string, *TopicConsumerGroups]
 }
