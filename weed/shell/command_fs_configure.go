682f24a9acc439fa613a0661ae6ea9331ab511c4
changlin.shi <changlin.shi@ly.com>
2023-12-25 11:39:28 +0800

    fs.conf 反序列化问题修复
    Signed-off-by: changlin.shi <changlin.shi@ly.com>

--- weed/shell/command_fs_configure.go
+++ weed/shell/command_fs_configure.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"strings"
 
 	"github.com/seaweedfs/seaweedfs/weed/filer"
 	"github.com/seaweedfs/seaweedfs/weed/pb/filer_pb"
@@ -51,7 +50,7 @@ func (c *commandFsConfigure) Do(args []string, commandEnv *CommandEnv, writer io
 	locationPrefix := fsConfigureCommand.String("locationPrefix", "", "path prefix, required to update the path-specific configuration")
 	collection := fsConfigureCommand.String("collection", "", "assign writes to this collection")
 	replication := fsConfigureCommand.String("replication", "", "assign writes with this replication")
-	ttl := fsConfigureCommand.String("ttl", "", "assign writes with this ttl")
+	ttl := fsConfigureCommand.String("ttl", "", "assign writes with this ttl (e.g., 1m, 1h, 1d, 1w, 1y)")
 	diskType := fsConfigureCommand.String("disk", "", "[hdd|ssd|<tag>] hard drive or solid state drive or any tag")
 	fsync := fsConfigureCommand.Bool("fsync", false, "fsync for the writes")
 	isReadOnly := fsConfigureCommand.Bool("readOnly", false, "disable writes")
@@ -84,11 +83,6 @@ func (c *commandFsConfigure) Do(args []string, commandEnv *CommandEnv, writer io
 			DataCenter:        *dataCenter,
 			Rack:              *rack,
 			DataNode:          *dataNode,
-		}
-
-		// check collection
-		if *collection != "" && strings.HasPrefix(*locationPrefix, "/buckets/") {
-			return fmt.Errorf("one s3 bucket goes to one collection and not customizable")
 		}
 
 		// check replication
