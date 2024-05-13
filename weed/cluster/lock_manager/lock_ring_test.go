c4dd105b97d6115f85f88920d25e06ca32fbbf6f
M <m@cognusion.com>
2024-05-13 08:36:30 -0400

    Fixes races with tests.
    Because of how sleeps are used and contrived, these tests only
    false-fail due to the lock races when the testing system is
    under heavy load, but using the read locks available mollifies
    those occurences too.

--- weed/cluster/lock_manager/lock_ring_test.go
+++ weed/cluster/lock_manager/lock_ring_test.go
@@ -19,25 +19,41 @@ func TestAddServer(t *testing.T) {
 	r.RemoveServer("localhost:8082")
 	r.RemoveServer("localhost:8080")
 
+	r.RLock()
 	assert.Equal(t, 8, len(r.snapshots))
+	r.RUnlock()
 
 	time.Sleep(110 * time.Millisecond)
 
+	r.RLock()
 	assert.Equal(t, 2, len(r.snapshots))
-
+	r.RUnlock()
 }
 
 func TestLockRing(t *testing.T) {
 	r := NewLockRing(100 * time.Millisecond)
 	r.SetSnapshot([]pb.ServerAddress{"localhost:8080", "localhost:8081"})
+	r.RLock()
 	assert.Equal(t, 1, len(r.snapshots))
+	r.RUnlock()
+
 	r.SetSnapshot([]pb.ServerAddress{"localhost:8080", "localhost:8081", "localhost:8082"})
+	r.RLock()
 	assert.Equal(t, 2, len(r.snapshots))
+	r.RUnlock()
 	time.Sleep(110 * time.Millisecond)
+
 	r.SetSnapshot([]pb.ServerAddress{"localhost:8080", "localhost:8081", "localhost:8082", "localhost:8083"})
+	r.RLock()
 	assert.Equal(t, 3, len(r.snapshots))
+	r.RUnlock()
 	time.Sleep(110 * time.Millisecond)
+	r.RLock()
 	assert.Equal(t, 2, len(r.snapshots))
+	r.RUnlock()
+
 	r.SetSnapshot([]pb.ServerAddress{"localhost:8080", "localhost:8081", "localhost:8082", "localhost:8083", "localhost:8084"})
+	r.RLock()
 	assert.Equal(t, 3, len(r.snapshots))
+	r.RUnlock()
 }
