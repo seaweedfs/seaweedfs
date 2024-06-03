160b5103612d4e5e96bbe12f9903466f54f0e828
Dominic Pearson <dsp@technoanimal.net>
2024-06-03 05:30:14 +0200

    Factor out common code, add divide-by-zero check

--- weed/stats/disk_openbsd.go
+++ weed/stats/disk_openbsd.go
@@ -15,10 +15,10 @@ func fillInDiskStatus(disk *volume_server_pb.DiskStatus) {
 	if err != nil {
 		return
 	}
+
 	disk.All = fs.F_blocks * uint64(fs.F_bsize)
 	disk.Free = fs.F_bfree * uint64(fs.F_bsize)
-	disk.Used = disk.All - disk.Free
-	disk.PercentFree = float32((float64(disk.Free) / float64(disk.All)) * 100)
-	disk.PercentUsed = float32((float64(disk.Used) / float64(disk.All)) * 100)
+	calculateDiskRemaining(disk)
+
 	return
 }
--- weed/stats/disk_solaris.go
+++ weed/stats/disk_solaris.go
@@ -11,7 +11,6 @@ import (
 
 func fillInDiskStatus(disk *volume_server_pb.DiskStatus) {
 	var stat unix.Statvfs_t
-
 	err := unix.Statvfs(disk.Dir, &stat)
 	if err != nil {
 		return
@@ -19,13 +18,7 @@ func fillInDiskStatus(disk *volume_server_pb.DiskStatus) {
 
 	disk.All = stat.Blocks * uint64(stat.Bsize)
 	disk.Free = stat.Bfree * uint64(stat.Bsize)
-	disk.Used = disk.All - disk.Free
+	calculateDiskRemaining(disk)
 
-	if disk.All > 0 {
-		disk.PercentFree = float32(float64(disk.Free) / float64(disk.All) * 100)
-		disk.PercentUsed = float32(float64(disk.Used) / float64(disk.All) * 100)
-	} else {
-		disk.PercentFree = 0
-		disk.PercentUsed = 0
-	}
+	return
 }
