282ad39e4d747aab90e0e0be72676b9c7bd45963
vadimartynov <vadim.martynov@flant.com>
2024-07-02 14:19:28 +0300

    Fixed name of disable_tls_verify_client_cert security param

--- weed/command/filer.go
+++ weed/command/filer.go
@@ -350,7 +350,7 @@ func (fo *FilerOptions) startFiler() {
 		certFile := viper.GetString("https.filer.cert")
 		keyFile := viper.GetString("https.filer.key")
 		caCertFile := viper.GetString("https.filer.ca")
-		disbaleTlsVerifyClientCert := viper.GetBool("https.filer.disbale_tls_verify_client_cert")
+		disbaleTlsVerifyClientCert := viper.GetBool("https.filer.disable_tls_verify_client_cert")
 
 		pemfileOptions := pemfile.Options{
 			CertFile:        certFile,
