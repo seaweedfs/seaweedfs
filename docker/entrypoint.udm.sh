16b4524b6d67e0d67dbfd86de82023baec644849
Bruce <half-life@jibudata.com>
2024-08-06 18:24:21 +0800

    Udm docker (#320)
    * add docker file for mysql and filer
    
    * update docker build
    
    * add docker compose for udm
    
    add docker file for udm multi arch build (#337)

---
+++ docker/entrypoint.udm.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+isArgPassed() {
+  arg="$1"
+  argWithEqualSign="$1="
+  shift
+  while [ $# -gt 0 ]; do
+    passedArg="$1"
+    shift
+    case $passedArg in
+    $arg)
+      return 0
+      ;;
+    $argWithEqualSign*)
+      return 0
+      ;;
+    esac
+  done
+  return 1
+}
+
+case "$1" in
+
+  'master')
+  	ARGS="-mdir=/data -volumePreallocate"
+  	shift
+  	exec /usr/bin/weed -logtostderr=true master $ARGS $@
+	;;
+
+  'volume')
+  	ARGS="-dir=/data -max=0"
+  	if isArgPassed "-max" "$@"; then
+  	  ARGS="-dir=/data"
+  	fi
+  	shift
+  	exec /usr/bin/weed -logtostderr=true volume $ARGS $@
+	;;
+
+  'server')
+  	ARGS="-dir=/data -volume.max=0 -master.volumePreallocate"
+  	if isArgPassed "-volume.max" "$@"; then
+  	  ARGS="-dir=/data -master.volumePreallocate"
+  	fi
+ 	shift
+  	exec /usr/bin/weed -logtostderr=true server $ARGS $@
+  	;;
+
+  'filer')
+  	ARGS=""
+  	shift
+  	exec /usr/bin/weed -logtostderr=true filer $ARGS $@
+	;;
+
+  's3')
+  	ARGS="-domainName=$S3_DOMAIN_NAME -key.file=$S3_KEY_FILE -cert.file=$S3_CERT_FILE"
+  	shift
+  	exec /usr/bin/weed -logtostderr=true s3 $ARGS $@
+	;;
+
+  'shell')
+  	ARGS="-cluster=$SHELL_CLUSTER -filer=$SHELL_FILER -filerGroup=$SHELL_FILER_GROUP -master=$SHELL_MASTER -options=$SHELL_OPTIONS"
+  	shift
+  	exec echo "$@" | /usr/bin/weed -logtostderr=true shell $ARGS
+  ;;
+
+  *)
+  	exec /usr/bin/weed $@
+	;;
+esac
