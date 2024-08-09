571d7066f134d9e18978a00b7234ed9f4382f6f9
Kamran Sarwar <kamransarwar47@gmail.com>
2024-08-10 00:46:06 +0500

    added access-control-expose-headers to put request
    added this header support to put request to facilitate access to response header keys in code.

--- weed/s3api/s3api_server.go
+++ weed/s3api/s3api_server.go
@@ -112,7 +112,7 @@ func (s3a *S3ApiServer) registerRouter(router *mux.Router) {
 	// Readiness Probe
 	apiRouter.Methods(http.MethodGet).Path("/status").HandlerFunc(s3a.StatusHandler)
 
-	apiRouter.Methods(http.MethodOptions).HandlerFunc(
+	apiRouter.Methods(http.MethodOptions, http.MethodPut).HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			origin := r.Header.Get("Origin")
 			if origin != "" {
