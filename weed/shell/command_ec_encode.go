089d4316ef3a2fd24acbdef732a92e24fdb6a27a
chrislu <chris.lu@gmail.com>
2024-10-24 22:44:53 -0700

    ensure 2 volume space since actual need 1.4x volume size empty space

--- weed/shell/command_ec_encode.go
+++ weed/shell/command_ec_encode.go
@@ -317,13 +317,13 @@ func collectVolumeIdsForEcEncode(commandEnv *CommandEnv, selectedCollection stri
 					if float64(v.Size) > fullPercentage/100*float64(volumeSizeLimitMb)*1024*1024 {
 						if good, found := vidMap[v.Id]; found {
 							if good {
-								if diskInfo.FreeVolumeCount < 1 {
+								if diskInfo.FreeVolumeCount < 2 {
 									glog.V(0).Infof("skip %d.%d on %s, no free disk", v.Id, v.Collection, dn.Id)
 									vidMap[v.Id] = false
 								}
 							}
 						} else {
-							if diskInfo.FreeVolumeCount < 1 {
+							if diskInfo.FreeVolumeCount < 2 {
 								glog.V(0).Infof("skip %d.%d on %s, no free disk", v.Id, v.Collection, dn.Id)
 								vidMap[v.Id] = false
 							} else {
