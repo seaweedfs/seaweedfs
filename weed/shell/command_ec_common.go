seaweedfs/seaweedfs

commit 1f736ce9c137b8a438d08847c85d5c5d1ea87080

Collect, and return, all errors on concurrent tasks for `shell.EcBalance()`.

weed/shell/command_ec_common.go
@@ -559,44 +559,46 @@ type ecBalancer struct {
 	applyBalancing   bool
 	parallelize      bool
 
-	wg *sync.WaitGroup
-	// TODO: Maybe accumulate all errors instead of just the last one.
-	wgError error
+	wg       *sync.WaitGroup
+	wgErrors []error
 }
 
 type ecBalancerTask func() error
 
 func (ecb *ecBalancer) wgInit() {
-	if ecb.wg == nil {
-		ecb.wg = &sync.WaitGroup{}
-		ecb.wgError = nil
+	if ecb.wg != nil {
+		return
 	}
+	ecb.wg = &sync.WaitGroup{}
+	ecb.wgErrors = nil
 }
 
 func (ecb *ecBalancer) wgAdd(f ecBalancerTask) {
-	if ecb.wg == nil || !ecb.parallelize {
-		if err := f(); err != nil {
-			ecb.wgError = err
+	wrapper := func() {
+		if ecb.wg != nil {
+			defer ecb.wg.Done()
 		}
+		if err := f(); err != nil {
+			ecb.wgErrors = append(ecb.wgErrors, err)
+		}
+	}
+
+	if ecb.wg == nil || !ecb.parallelize {
+		wrapper()
 		return
 	}
 
 	ecb.wg.Add(1)
-	go func() {
-		if err := f(); err != nil {
-			ecb.wgError = err
-		}
-		ecb.wg.Done()
-	}()
+	go wrapper()
 }
 
 func (ecb *ecBalancer) wgWait() error {
 	if ecb.wg != nil {
 		ecb.wg.Wait()
 	}
-	err := ecb.wgError
+	err := errors.Join(ecb.wgErrors...)
 	ecb.wg = nil
-	ecb.wgError = nil
+	ecb.wgErrors = nil
 
 	return err
 }

commit c25ed98665f040a8f7947a2cad9b8f13ac2c1f62

Fix EC concurrency bug introduced by 1f736ce9.
Premature optimization is a thing 🤦

weed/shell/command_ec_common.go
@@ -574,22 +574,20 @@ func (ecb *ecBalancer) wgInit() {
 }
 
 func (ecb *ecBalancer) wgAdd(f ecBalancerTask) {
-	wrapper := func() {
-		if ecb.wg != nil {
-			defer ecb.wg.Done()
-		}
+	if ecb.wg == nil || !ecb.parallelize {
 		if err := f(); err != nil {
 			ecb.wgErrors = append(ecb.wgErrors, err)
 		}
-	}
-
-	if ecb.wg == nil || !ecb.parallelize {
-		wrapper()
 		return
 	}
 
 	ecb.wg.Add(1)
-	go wrapper()
+	go func() {
+		if err := f(); err != nil {
+			ecb.wgErrors = append(ecb.wgErrors, err)
+		}
+		ecb.wg.Done()
+	}()
 }
 
 func (ecb *ecBalancer) wgWait() error {