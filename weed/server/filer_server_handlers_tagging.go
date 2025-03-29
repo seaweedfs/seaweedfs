d5216bf54f67b0e9987c7f6c005fc260d84de7b0
alingse <alingse@foxmail.com>
2025-03-29 23:12:22 +0800

    fix call writeJsonError with a nil value error after check another err
    the dbErr != nil and err is nil
    
    when call writeJsonError with a nil value error, it may panic

--- weed/server/filer_server_handlers_tagging.go
+++ weed/server/filer_server_handlers_tagging.go
@@ -45,7 +45,7 @@ func (fs *FilerServer) PutTaggingHandler(w http.ResponseWriter, r *http.Request)
 
 	if dbErr := fs.filer.CreateEntry(ctx, existingEntry, false, false, nil, false, fs.filer.MaxFilenameLength); dbErr != nil {
 		glog.V(0).Infof("failing to update %s tagging : %v", path, dbErr)
-		writeJsonError(w, r, http.StatusInternalServerError, err)
+		writeJsonError(w, r, http.StatusInternalServerError, dbErr)
 		return
 	}
 
@@ -111,7 +111,7 @@ func (fs *FilerServer) DeleteTaggingHandler(w http.ResponseWriter, r *http.Reque
 
 	if dbErr := fs.filer.CreateEntry(ctx, existingEntry, false, false, nil, false, fs.filer.MaxFilenameLength); dbErr != nil {
 		glog.V(0).Infof("failing to delete %s tagging : %v", path, dbErr)
-		writeJsonError(w, r, http.StatusInternalServerError, err)
+		writeJsonError(w, r, http.StatusInternalServerError, dbErr)
 		return
 	}
 
