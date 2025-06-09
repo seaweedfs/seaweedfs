81aeec74a4eeb2c952c1038b39545c8d77a93ad9
chrislu <chris.lu@gmail.com>
2025-06-08 20:59:43 -0700

    3.90

--- weed/util/version/constants.go
+++ weed/util/version/constants.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"fmt"
+
 	"github.com/seaweedfs/seaweedfs/weed/util"
 )
 
@@ -9,7 +10,7 @@ const HttpStatusCancelled = 499
 
 var (
 	MAJOR_VERSION  = int32(3)
-	MINOR_VERSION  = int32(89)
+	MINOR_VERSION  = int32(90)
 	VERSION_NUMBER = fmt.Sprintf("%d.%02d", MAJOR_VERSION, MINOR_VERSION)
 	VERSION        = util.SizeLimit + " " + VERSION_NUMBER
 	COMMIT         = ""
