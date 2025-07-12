6736434176f86c6e222b867777324b17c2de716f
chrislu <chris.lu@gmail.com>
2025-07-12 12:10:45 -0700

    comments

--- weed/s3api/s3api_object_handlers_skip.go
+++ weed/s3api/s3api_object_handlers_skip.go
@@ -4,18 +4,16 @@ import (
 	"net/http"
 )
 
-// GetObjectAclHandler Put object ACL
+// GetObjectAclHandler Get object ACL
 // https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetObjectAcl.html
 func (s3a *S3ApiServer) GetObjectAclHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.WriteHeader(http.StatusNoContent)
-
+	// TODO: Implement
+	w.WriteHeader(http.StatusNotImplemented)
 }
 
 // PutObjectAclHandler Put object ACL
 // https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutObjectAcl.html
 func (s3a *S3ApiServer) PutObjectAclHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.WriteHeader(http.StatusNoContent)
-
+	// TODO: Implement
+	w.WriteHeader(http.StatusNotImplemented)
 }
