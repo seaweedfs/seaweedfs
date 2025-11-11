bf8e4f40e60e74ce03c2f497c6245e5d1460f1d3
Chris Lu <chrislusf@users.noreply.github.com>
2025-11-10 20:30:21 -0800

    S3: Perf related (#7463)
    * reduce checks
    
    * s3 object lookup optimization
    
    * Only check versioning configuration if client requests
    * Consolidate SSE Entry Lookups
    
    * optimize
    
    * revert optimization for versioned objects
    
    * Removed: getObjectEntryForSSE() function
    
    * refactor
    
    * Refactoring: Added fetchObjectEntryRequired
    
    * avoid refetching
    
    * return early if not found
    
    * reuse objects from conditional check
    
    * clear cache when creating bucket

--- weed/s3api/custom_types.go
+++ weed/s3api/custom_types.go
@@ -1,11 +1,15 @@
 package s3api
 
-import "github.com/seaweedfs/seaweedfs/weed/s3api/s3err"
+import (
+	"github.com/seaweedfs/seaweedfs/weed/pb/filer_pb"
+	"github.com/seaweedfs/seaweedfs/weed/s3api/s3err"
+)
 
 const s3TimeFormat = "2006-01-02T15:04:05.999Z07:00"
 
 // ConditionalHeaderResult holds the result of conditional header checking
 type ConditionalHeaderResult struct {
 	ErrorCode s3err.ErrorCode
-	ETag      string // ETag of the object (for 304 responses)
+	ETag      string           // ETag of the object (for 304 responses)
+	Entry     *filer_pb.Entry  // Entry fetched during conditional check (nil if not fetched or object doesn't exist)
 }
