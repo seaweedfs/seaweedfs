08ae6e8ca45842a13af726dec1e23bc35f75f906
chrislu <chris.lu@gmail.com>
2025-11-19 13:48:39 -0800

    address comments

--- docker/get_fdb_checksum.sh
+++ docker/get_fdb_checksum.sh
@@ -16,12 +16,22 @@ fi
 FDB_VERSION="$1"
 FDB_ARCH="${2:-amd64}"
 
-if [ "$FDB_ARCH" != "amd64" ] && [ "$FDB_ARCH" != "arm64" ]; then
-    echo "Error: Architecture must be 'amd64' or 'arm64'" >&2
-    exit 1
-fi
+case "$FDB_ARCH" in
+    "amd64")
+        CANONICAL_ARCH="amd64"
+        PACKAGE_ARCH="amd64"
+        ;;
+    "arm64"|"aarch64")
+        CANONICAL_ARCH="arm64"
+        PACKAGE_ARCH="aarch64"
+        ;;
+    *)
+        echo "Error: Architecture must be 'amd64', 'arm64', or 'aarch64'" >&2
+        exit 1
+        ;;
+esac
 
-PACKAGE="foundationdb-clients_${FDB_VERSION}-1_${FDB_ARCH}.deb"
+PACKAGE="foundationdb-clients_${FDB_VERSION}-1_${PACKAGE_ARCH}.deb"
 URL="https://github.com/apple/foundationdb/releases/download/${FDB_VERSION}/${PACKAGE}"
 
 echo "Downloading FoundationDB ${FDB_VERSION} client package for ${FDB_ARCH}..."
@@ -41,7 +51,7 @@ if wget --timeout=30 --tries=3 -q "${URL}"; then
     echo "${CHECKSUM}"
     echo ""
     echo "Add this to Dockerfile.foundationdb_large:"
-    echo "    \"${FDB_VERSION}_${FDB_ARCH}\") \\"
+    echo "    \"${FDB_VERSION}_${CANONICAL_ARCH}\") \\"
     echo "        EXPECTED_SHA256=\"${CHECKSUM}\" ;; \\"
 else
     echo "✗ Failed to download package from ${URL}" >&2
