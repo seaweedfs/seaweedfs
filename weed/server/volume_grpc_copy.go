c9734d2d114f58c3f43c52411f4fa73e87e88ecd
chrislu <chris.lu@gmail.com>
2025-12-09 12:24:51 -0800

    refactor: remove redundant dst.Close() and add error logging
    Address review feedback:
    - Remove redundant dst.Close() call since defer already handles it
    - Add error logging for os.Remove() failure

--- weed/server/volume_grpc_copy.go
+++ weed/server/volume_grpc_copy.go
@@ -294,9 +294,11 @@ func writeToFile(client volume_server_pb.VolumeServer_CopyFileClient, fileName s
 	// If no data was written (source file was not found), remove the empty file
 	// to avoid leaving corrupted empty files that cause parse errors later
 	if progressedBytes == 0 && !isAppend {
-		dst.Close()
-		os.Remove(fileName)
-		glog.V(1).Infof("removed empty file %s (source file not found)", fileName)
+		if removeErr := os.Remove(fileName); removeErr != nil {
+			glog.V(1).Infof("failed to remove empty file %s: %v", fileName, removeErr)
+		} else {
+			glog.V(1).Infof("removed empty file %s (source file not found)", fileName)
+		}
 	}
 	return modifiedTsNs, nil
 }
