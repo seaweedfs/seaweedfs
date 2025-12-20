2ea100a481e4833de21d9eeecfd7c8086557be4c
Chris Lu <chris.lu@gmail.com>
2025-12-20 14:52:48 -0800

    S3 metrics: reduce collection interval to half of bucketSizeMetricsInterval
    Since S3 metrics collection is not critical, check more frequently but
    only collect when holding the distributed lock. This allows faster
    detection of any issues while avoiding overhead on non-leader instances.

--- weed/s3api/bucket_size_metrics.go
+++ weed/s3api/bucket_size_metrics.go
@@ -62,10 +62,10 @@ func (s3a *S3ApiServer) startBucketSizeMetricsLoop(ctx context.Context) {
 	// Start long-lived lock - this S3 instance will only collect metrics when it holds the lock
 	lock := lockClient.StartLongLivedLock(s3MetricsLockName, owner, func(newLockOwner string) {
 		glog.V(1).Infof("S3 bucket size metrics lock owner changed to: %s", newLockOwner)
-	}, lock_manager.RenewInterval)
+	}, bucketSizeMetricsInterval/2)
 	defer lock.Stop()
 
-	ticker := time.NewTicker(bucketSizeMetricsInterval)
+	ticker := time.NewTicker(bucketSizeMetricsInterval / 2)
 	defer ticker.Stop()
 
 	for {
