2dfc29077da281fb10f7fdefb4af082f2d401182
Chris Lu <chris.lu@gmail.com>
2025-12-29 11:10:16 -0800

    iam: remove unused UserAccessKeyPrefix constant
    Code cleanup to remove unused constants.

--- weed/s3api/s3api_embedded_iam.go
+++ weed/s3api/s3api_embedded_iam.go
@@ -50,7 +50,6 @@ const (
 	SecretKeyLength         = 40 // AWS standard secret key length (base64 encoded)
 	ServiceAccountIDPrefix  = "sa"
 	ServiceAccountKeyPrefix = "ABIA" // Service account access keys start with ABIA
-	UserAccessKeyPrefix     = "AKIA" // User access keys start with AKIA
 )
 
 // Type aliases for IAM response types from shared package
