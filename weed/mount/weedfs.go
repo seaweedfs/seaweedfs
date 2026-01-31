2f3a8e3a6b952354efcfa7c2e9ca6ff4925839e7
Chris Lu <chris.lu@gmail.com>
2026-01-31 10:59:14 -0800

    mount: clear cache under mount root on retry

--- weed/mount/weedfs.go
+++ weed/mount/weedfs.go
@@ -217,7 +217,7 @@ func (wfs *WFS) StartBackgroundTasks() error {
 	startTime := time.Now()
 	go meta_cache.SubscribeMetaEvents(wfs.metaCache, wfs.signature, wfs, wfs.option.FilerMountRootPath, startTime.UnixNano(), func(lastTsNs int64, err error) {
 		glog.Warningf("meta events follow retry from %v: %v", time.Unix(0, lastTsNs), err)
-		if deleteErr := wfs.metaCache.DeleteFolderChildren(context.Background(), util.FullPath("/")); deleteErr != nil {
+		if deleteErr := wfs.metaCache.DeleteFolderChildren(context.Background(), util.FullPath(wfs.option.FilerMountRootPath)); deleteErr != nil {
 			glog.Warningf("meta cache cleanup failed: %v", deleteErr)
 		}
 		wfs.inodeToPath.InvalidateAllChildrenCache()
