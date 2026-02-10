af8273386dce51cab7862f9c8f825f82be1e56ef
Chris Lu <chris.lu@gmail.com>
2026-02-09 18:15:19 -0800

    4.12

--- weed/util/version/constants.go
+++ weed/util/version/constants.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	MAJOR_VERSION  = int32(4)
-	MINOR_VERSION  = int32(11)
+	MINOR_VERSION  = int32(12)
 	VERSION_NUMBER = fmt.Sprintf("%d.%02d", MAJOR_VERSION, MINOR_VERSION)
 	VERSION        = util.SizeLimit + " " + VERSION_NUMBER
 	COMMIT         = ""
