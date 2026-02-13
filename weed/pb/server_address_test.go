a9d5e8f5a0f0ec7d20fe4de9dfdb2ea890f17bb9
Chris Lu <chris.lu@gmail.com>
2026-02-12 17:14:51 -0800

    pb: fix IPv6 double brackets in ServerAddress formatting

--- weed/pb/server_address_test.go
+++ weed/pb/server_address_test.go
@@ -34,3 +34,36 @@ func TestServerAddresses_ToAddressMapOrSrv_shouldHandleIPPortList(t *testing.T) 
 		t.Fatalf(`Expected %q, got %q`, expected, d.list)
 	}
 }
+
+func TestIPv6ServerAddress(t *testing.T) {
+	host := "2001:db8::1"
+	port := 8080
+	grpcPort := 18080
+	sa := NewServerAddress(host, port, grpcPort)
+
+	expectedHttp := "[2001:db8::1]:8080"
+	if httpAddr := sa.ToHttpAddress(); httpAddr != expectedHttp {
+		t.Errorf("ToHttpAddress() = %s, want %s", httpAddr, expectedHttp)
+	}
+
+	expectedGrpc := "[2001:db8::1]:18080"
+	if grpcAddr := sa.ToGrpcAddress(); grpcAddr != expectedGrpc {
+		t.Errorf("ToGrpcAddress() = %s, want %s", grpcAddr, expectedGrpc)
+	}
+}
+
+func TestIPv6ServerAddressWithBrackets(t *testing.T) {
+	host := "[2001:db8::1]"
+	grpcPort := 18080
+	sa := NewServerAddressWithGrpcPort(host+":8080", grpcPort)
+
+	expectedHttp := "[2001:db8::1]:8080"
+	if httpAddr := sa.ToHttpAddress(); httpAddr != expectedHttp {
+		t.Errorf("ToHttpAddress() = %s, want %s", httpAddr, expectedHttp)
+	}
+
+	expectedGrpc := "[2001:db8::1]:18080"
+	if grpcAddr := sa.ToGrpcAddress(); grpcAddr != expectedGrpc {
+		t.Errorf("ToGrpcAddress() = %s, want %s", grpcAddr, expectedGrpc)
+	}
+}
