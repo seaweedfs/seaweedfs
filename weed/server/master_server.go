ac6022fc242119b45075a27195ceaa7e2209cb87
Chris Lu <chris.lu@gmail.com>
2026-02-16 16:42:34 -0800

    filer: fix default maintenance scripts to include full set of tasks

--- weed/server/master_server.go
+++ weed/server/master_server.go
@@ -312,7 +312,18 @@ func (ms *MasterServer) proxyToLeader(f http.HandlerFunc) http.HandlerFunc {
 
 func (ms *MasterServer) startAdminScripts() {
 	v := util.GetViper()
-	v.SetDefault("master.maintenance.scripts", "fs.log.purge -daysAgo=7")
+	v.SetDefault("master.maintenance.scripts", `
+  lock
+  ec.encode -fullPercent=95 -quietFor=1h
+  ec.rebuild -apply
+  ec.balance -apply
+  fs.log.purge -daysAgo=7
+  volume.deleteEmpty -quietFor=24h -apply
+  volume.balance -apply
+  volume.fix.replication -apply
+  s3.clean.uploads -timeAgo=24h
+  unlock
+`)
 	adminScripts := v.GetString("master.maintenance.scripts")
 	if adminScripts == "" {
 		return
