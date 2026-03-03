7881e373dc7f9168dee79118909112c86f75d22a
Chris Lu <chris.lu@gmail.com>
2026-03-03 13:29:34 -0800

    shell: restrict sleep syntax

--- weed/shell/command_sleep.go
+++ weed/shell/command_sleep.go
@@ -21,11 +21,9 @@ func (c *commandSleep) Name() string {
 }
 
 func (c *commandSleep) Help() string {
-	return `sleep for a duration (useful to simulate long running jobs)
+	return `sleep for N seconds (useful to simulate long running jobs)
 
-	sleep -seconds 5
-	sleep -duration 2s
-	sleep 1500ms
+	sleep 5
 `
 }
 
@@ -34,38 +32,13 @@ func (c *commandSleep) HasTag(CommandTag) bool {
 }
 
 func (c *commandSleep) Do(args []string, _ *CommandEnv, _ io.Writer) error {
-	sleepCommand := flag.NewFlagSet(c.Name(), flag.ContinueOnError)
-	seconds := sleepCommand.Int("seconds", 0, "sleep seconds")
-	duration := sleepCommand.Duration("duration", 0, "sleep duration (e.g. 2s, 1m)")
-	if err := sleepCommand.Parse(args); err != nil {
-		return err
+	if len(args) == 0 {
+		return fmt.Errorf("sleep requires a seconds argument")
 	}
-
-	var sleepFor time.Duration
-	if *duration > 0 {
-		sleepFor = *duration
-	} else if *seconds > 0 {
-		sleepFor = time.Duration(*seconds) * time.Second
-	} else {
-		remaining := sleepCommand.Args()
-		if len(remaining) > 0 {
-			value := remaining[0]
-			parsed, parseErr := time.ParseDuration(value)
-			if parseErr != nil {
-				if asInt, intErr := strconv.Atoi(value); intErr == nil {
-					parsed = time.Duration(asInt) * time.Second
-				} else {
-					return fmt.Errorf("invalid duration %q", value)
-				}
-			}
-			sleepFor = parsed
-		}
+	seconds, err := strconv.Atoi(args[0])
+	if err != nil || seconds <= 0 {
+		return fmt.Errorf("sleep duration must be a positive integer, got %q", args[0])
 	}
-
-	if sleepFor <= 0 {
-		return fmt.Errorf("sleep duration is required")
-	}
-
-	time.Sleep(sleepFor)
+	time.Sleep(time.Duration(seconds) * time.Second)
 	return nil
 }
