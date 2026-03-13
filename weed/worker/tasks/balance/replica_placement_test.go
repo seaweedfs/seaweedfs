ce1475b40ff7ab358a9d42195ab50eacbf4b8b69
Chris Lu <chris.lu@gmail.com>
2026-03-13 15:42:45 -0700

    address nitpick: rp test helper accepts *testing.T and fails on error
    Prevents silent failures from typos in replica placement codes.

--- weed/worker/tasks/balance/replica_placement_test.go
+++ weed/worker/tasks/balance/replica_placement_test.go
@@ -7,8 +7,12 @@ import (
 	"github.com/seaweedfs/seaweedfs/weed/worker/types"
 )
 
-func rp(code string) *super_block.ReplicaPlacement {
-	r, _ := super_block.NewReplicaPlacementFromString(code)
+func rp(t *testing.T, code string) *super_block.ReplicaPlacement {
+	t.Helper()
+	r, err := super_block.NewReplicaPlacementFromString(code)
+	if err != nil {
+		t.Fatalf("invalid replica placement code %q: %v", code, err)
+	}
 	return r
 }
 
@@ -18,7 +22,7 @@ func loc(dc, rack, node string) types.ReplicaLocation {
 
 func TestIsGoodMove_NoReplication(t *testing.T) {
 	// 000 = no replication. Any move is fine.
-	if !IsGoodMove(rp("000"), []types.ReplicaLocation{loc("dc1", "r1", "n1")}, "n1", loc("dc1", "r1", "n2")) {
+	if !IsGoodMove(rp(t, "000"), []types.ReplicaLocation{loc("dc1", "r1", "n1")}, "n1", loc("dc1", "r1", "n2")) {
 		t.Error("000: any move should be allowed")
 	}
 }
@@ -30,11 +34,11 @@ func TestIsGoodMove_001_SameRack(t *testing.T) {
 		loc("dc1", "r1", "n2"),
 	}
 	// Move n1 -> n3 on same rack: good
-	if !IsGoodMove(rp("001"), existing, "n1", loc("dc1", "r1", "n3")) {
+	if !IsGoodMove(rp(t, "001"), existing, "n1", loc("dc1", "r1", "n3")) {
 		t.Error("001: move to same rack should be allowed")
 	}
 	// Move n1 -> n3 on different rack: bad (would leave only 1 on r1, need 2)
-	if IsGoodMove(rp("001"), existing, "n1", loc("dc1", "r2", "n3")) {
+	if IsGoodMove(rp(t, "001"), existing, "n1", loc("dc1", "r2", "n3")) {
 		t.Error("001: move to different rack should not be allowed when it breaks same-rack count")
 	}
 }
@@ -46,11 +50,11 @@ func TestIsGoodMove_010_DiffRack(t *testing.T) {
 		loc("dc1", "r2", "n2"),
 	}
 	// Move n1 -> n3 on r2: bad (both replicas on same rack)
-	if IsGoodMove(rp("010"), existing, "n1", loc("dc1", "r2", "n3")) {
+	if IsGoodMove(rp(t, "010"), existing, "n1", loc("dc1", "r2", "n3")) {
 		t.Error("010: move to same rack as other replica should not be allowed")
 	}
 	// Move n1 -> n3 on r3: good (still 2 different racks)
-	if !IsGoodMove(rp("010"), existing, "n1", loc("dc1", "r3", "n3")) {
+	if !IsGoodMove(rp(t, "010"), existing, "n1", loc("dc1", "r3", "n3")) {
 		t.Error("010: move to different rack should be allowed")
 	}
 }
@@ -62,11 +66,11 @@ func TestIsGoodMove_100_DiffDC(t *testing.T) {
 		loc("dc2", "r1", "n2"),
 	}
 	// Move n1 -> n3 in dc2: bad (both in same DC)
-	if IsGoodMove(rp("100"), existing, "n1", loc("dc2", "r1", "n3")) {
+	if IsGoodMove(rp(t, "100"), existing, "n1", loc("dc2", "r1", "n3")) {
 		t.Error("100: move to same DC as other replica should not be allowed")
 	}
 	// Move n1 -> n3 in dc3: good (different DCs)
-	if !IsGoodMove(rp("100"), existing, "n1", loc("dc3", "r1", "n3")) {
+	if !IsGoodMove(rp(t, "100"), existing, "n1", loc("dc3", "r1", "n3")) {
 		t.Error("100: move to different DC should be allowed")
 	}
 }
@@ -77,7 +81,7 @@ func TestIsGoodMove_SameNode(t *testing.T) {
 		loc("dc1", "r1", "n1"),
 		loc("dc1", "r2", "n2"),
 	}
-	if IsGoodMove(rp("010"), existing, "n1", loc("dc1", "r2", "n2")) {
+	if IsGoodMove(rp(t, "010"), existing, "n1", loc("dc1", "r2", "n2")) {
 		t.Error("should reject move to same node as existing replica")
 	}
 }
