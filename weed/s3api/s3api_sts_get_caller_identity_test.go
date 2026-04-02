bd3ee4a4adf38f9e46773b4657914cbc62a4457b
Chris Lu <chris.lu@gmail.com>
2026-04-02 13:53:05 -0700

    test: remove trivial GetCallerIdentity tests
    Remove the XML unmarshal test (we don't consume this response as input)
    and the routing constant test (just asserts a literal equals itself).

--- weed/s3api/s3api_sts_get_caller_identity_test.go
+++ weed/s3api/s3api_sts_get_caller_identity_test.go
@@ -31,30 +31,3 @@ func TestGetCallerIdentityResponse_XMLMarshal(t *testing.T) {
 	assert.Contains(t, xmlStr, "<RequestId>test-request-id</RequestId>")
 	assert.Contains(t, xmlStr, "https://sts.amazonaws.com/doc/2011-06-15/")
 }
-
-func TestGetCallerIdentityResponse_XMLUnmarshal(t *testing.T) {
-	xmlData := `<GetCallerIdentityResponse xmlns="https://sts.amazonaws.com/doc/2011-06-15/">
-  <GetCallerIdentityResult>
-    <Arn>arn:aws:iam::000000000000:user/bob</Arn>
-    <UserId>AKIAI44QH8DHBEXAMPLE</UserId>
-    <Account>000000000000</Account>
-  </GetCallerIdentityResult>
-  <ResponseMetadata>
-    <RequestId>req-123</RequestId>
-  </ResponseMetadata>
-</GetCallerIdentityResponse>`
-
-	var response GetCallerIdentityResponse
-	err := xml.Unmarshal([]byte(xmlData), &response)
-	require.NoError(t, err)
-
-	assert.Equal(t, "arn:aws:iam::000000000000:user/bob", response.Result.Arn)
-	assert.Equal(t, "AKIAI44QH8DHBEXAMPLE", response.Result.UserId)
-	assert.Equal(t, "000000000000", response.Result.Account)
-	assert.Equal(t, "req-123", response.ResponseMetadata.RequestId)
-}
-
-func TestGetCallerIdentity_HandleSTSRequest_Routing(t *testing.T) {
-	// Verify the action constant is correct
-	assert.Equal(t, "GetCallerIdentity", actionGetCallerIdentity)
-}
