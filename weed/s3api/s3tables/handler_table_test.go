54b1c280ccf973337a97a783b1773a3682aafd97
Chris Lu <chris.lu@gmail.com>
2026-06-23 11:06:55 -0700

    iceberg: test metadata version parsing for both filename forms

--- weed/s3api/s3tables/handler_table_test.go
+++ weed/s3api/s3tables/handler_table_test.go
@@ -10,8 +10,11 @@ func TestMetadataVersionFromLocation(t *testing.T) {
 		{"s3://bucket/ns/tbl/metadata/v1.metadata.json", 1},
 		{"s3://bucket/ns/tbl/metadata/v7.metadata.json", 7},
 		{"v42.metadata.json", 42},
+		{"s3://bucket/ns/tbl/metadata/00003-9f1c2b3a-4d5e-6f70-8192-a3b4c5d6e7f8.metadata.json", 3},
+		{"00012-abcdef.metadata.json", 12},
 		{"s3://bucket/ns/tbl/metadata/00000-abc.metadata.json", 1},
 		{"s3://bucket/ns/tbl/metadata/v0.metadata.json", 1},
+		{"s3://bucket/ns/tbl/metadata/garbage.metadata.json", 1},
 		{"", 1},
 	}
 	for _, c := range cases {
