fc3c5bd8be9d81544660c4d49ccb8549a05918fc
Marat Karimov <karimov_m@inbox.ru>
2026-07-20 22:19:16 +0300

    Fix review comments

--- weed/filer/tarantool/tarantool_slog.go
+++ weed/filer/tarantool/tarantool_slog.go
@@ -13,7 +13,9 @@ import (
 
 // glogSlogHandler forwards go-tarantool/v3's structured logs into weed/glog,
 // which is the logging facility used everywhere else in SeaweedFS.
-type glogSlogHandler struct{}
+type glogSlogHandler struct {
+	preformatted string
+}
 
 func newGlogLogger() *slog.Logger {
 	return slog.New(&glogSlogHandler{})
@@ -26,6 +28,9 @@ func (h *glogSlogHandler) Enabled(context.Context, slog.Level) bool {
 func (h *glogSlogHandler) Handle(ctx context.Context, r slog.Record) error {
 	msg := r.Message
 	var attrs []string
+	if h.preformatted != "" {
+		attrs = append(attrs, h.preformatted)
+	}
 	r.Attrs(func(a slog.Attr) bool {
 		attrs = append(attrs, fmt.Sprintf("%s=%v", a.Key, a.Value.Any()))
 		return true
@@ -39,6 +44,8 @@ func (h *glogSlogHandler) Handle(ctx context.Context, r slog.Record) error {
 		glog.ErrorfCtx(ctx, "tarantool: %s", msg)
 	case r.Level >= slog.LevelWarn:
 		glog.WarningfCtx(ctx, "tarantool: %s", msg)
+	case r.Level >= slog.LevelInfo:
+		glog.InfofCtx(ctx, "tarantool: %s", msg)
 	default:
 		glog.V(1).InfofCtx(ctx, "tarantool: %s", msg)
 	}
@@ -46,7 +53,21 @@ func (h *glogSlogHandler) Handle(ctx context.Context, r slog.Record) error {
 }
 
 func (h *glogSlogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return h
+	if len(attrs) == 0 {
+		return h
+	}
+	var sb strings.Builder
+	if h.preformatted != "" {
+		sb.WriteString(h.preformatted)
+		sb.WriteString(" ")
+	}
+	for i, a := range attrs {
+		if i > 0 {
+			sb.WriteString(" ")
+		}
+		sb.WriteString(fmt.Sprintf("%s=%v", a.Key, a.Value.Any()))
+	}
+	return &glogSlogHandler{preformatted: sb.String()}
 }
 
 func (h *glogSlogHandler) WithGroup(name string) slog.Handler {
