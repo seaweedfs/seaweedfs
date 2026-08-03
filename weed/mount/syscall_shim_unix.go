f409c2588f2c26f786e317a95f0d14effcc22646
Chris Lu <chris.lu@gmail.com>
2026-08-03 09:11:04 -0700

    mount: keep the xattr flag constants off freebsd
    x/sys/unix has no XATTR_CREATE or XATTR_REPLACE there, and weedfs_xattr.go
    is already tagged away from freebsd for that reason. Putting the constants
    in a !windows file dragged them back in and broke the freebsd build.

--- weed/mount/syscall_shim_unix.go
+++ weed/mount/syscall_shim_unix.go
@@ -2,18 +2,11 @@
 
 package mount
 
-import (
-	"syscall"
-
-	sys "golang.org/x/sys/unix"
-)
+import "syscall"
 
 const (
 	f_RDLCK   = syscall.F_RDLCK
 	f_WRLCK   = syscall.F_WRLCK
 	f_UNLCK   = syscall.F_UNLCK
 	o_ACCMODE = syscall.O_ACCMODE
-
-	xattr_CREATE  = sys.XATTR_CREATE
-	xattr_REPLACE = sys.XATTR_REPLACE
 )
