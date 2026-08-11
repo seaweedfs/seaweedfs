6b24a5f25a8fcccbb756cbf9742652d48f0135bb
Chris Lu <chris.lu@gmail.com>
2026-08-11 16:51:56 -0700

    remote_storage: build S3-compatible clients through one constructor
    The eight non-s3 S3-SDK providers each duplicated the AWS session setup
    and only the s3 maker could take a custom *http.Client. Route every
    S3-compatible type (s3, wasabi, b2, aliyun, tencent, baidu, filebase,
    storj, contabo) through MakeWithHTTPClient with a single options table,
    and add S3CompatibleEndpoint so callers can resolve the endpoint a given
    type dials. No behavior change.

--- weed/remote_storage/s3/storj.go
+++ weed/remote_storage/s3/storj.go
@@ -1,16 +1,8 @@
 package s3
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/seaweedfs/seaweedfs/weed/pb/remote_pb"
 	"github.com/seaweedfs/seaweedfs/weed/remote_storage"
-	"github.com/seaweedfs/seaweedfs/weed/util"
 )
 
 func init() {
@@ -24,27 +16,5 @@ func (s StorjRemoteStorageMaker) HasBucket() bool {
 }
 
 func (s StorjRemoteStorageMaker) Make(conf *remote_pb.RemoteConf) (remote_storage.RemoteStorageClient, error) {
-	client := &s3RemoteStorageClient{
-		conf: conf,
-	}
-	accessKey := util.Nvl(conf.StorjAccessKey, os.Getenv("AWS_ACCESS_KEY_ID"))
-	secretKey := util.Nvl(conf.StorjSecretKey, os.Getenv("AWS_SECRET_ACCESS_KEY"))
-
-	config := &aws.Config{
-		Endpoint:                      aws.String(conf.StorjEndpoint),
-		Region:                        aws.String("us-west-2"),
-		S3ForcePathStyle:              aws.Bool(true),
-		S3DisableContentMD5Validation: aws.Bool(true),
-	}
-	if accessKey != "" && secretKey != "" {
-		config.Credentials = credentials.NewStaticCredentials(accessKey, secretKey, "")
-	}
-
-	sess, err := session.NewSession(config)
-	if err != nil {
-		return nil, fmt.Errorf("create storj session: %w", err)
-	}
-	sess.Handlers.Build.PushFront(skipSha256PayloadSigning)
-	client.conn = s3.New(sess)
-	return client, nil
+	return MakeWithHTTPClient(conf, nil)
 }
--- weed/remote_storage/s3/wasabi.go
+++ weed/remote_storage/s3/wasabi.go
@@ -1,16 +1,8 @@
 package s3
 
 import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/seaweedfs/seaweedfs/weed/pb/remote_pb"
 	"github.com/seaweedfs/seaweedfs/weed/remote_storage"
-	"github.com/seaweedfs/seaweedfs/weed/util"
 )
 
 func init() {
@@ -24,39 +16,5 @@ func (s WasabiRemoteStorageMaker) HasBucket() bool {
 }
 
 func (s WasabiRemoteStorageMaker) Make(conf *remote_pb.RemoteConf) (remote_storage.RemoteStorageClient, error) {
-	client := &s3RemoteStorageClient{
-		conf: conf,
-	}
-	accessKey := util.Nvl(conf.WasabiAccessKey)
-	secretKey := util.Nvl(conf.WasabiSecretKey)
-
-	config := &aws.Config{
-		Endpoint:                      aws.String(conf.WasabiEndpoint),
-		Region:                        aws.String(conf.WasabiRegion),
-		S3ForcePathStyle:              aws.Bool(true),
-		S3DisableContentMD5Validation: aws.Bool(true),
-	}
-	if accessKey != "" && secretKey != "" {
-		config.Credentials = credentials.NewStaticCredentials(accessKey, secretKey, "")
-	}
-
-	sess, err := session.NewSession(config)
-	if err != nil {
-		return nil, fmt.Errorf("create wasabi session: %w", err)
-	}
-	sess.Handlers.Build.PushFront(skipSha256PayloadSigning)
-	client.conn = s3.New(sess)
-	return client, nil
-}
-
-var skipSha256PayloadSigning = func(r *request.Request) {
-	// see https://github.com/ceph/ceph/pull/15965/files
-	if r.ClientInfo.ServiceID != "S3" {
-		return
-	}
-	if r.Operation.Name == "PutObject" || r.Operation.Name == "UploadPart" {
-		if len(r.HTTPRequest.Header.Get("X-Amz-Content-Sha256")) == 0 {
-			r.HTTPRequest.Header.Set("X-Amz-Content-Sha256", "UNSIGNED-PAYLOAD")
-		}
-	}
+	return MakeWithHTTPClient(conf, nil)
 }
