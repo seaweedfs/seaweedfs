17c4e0a7d2a0f16529caaa941bcf8d9fa0b64a33
Chris Lu <chris.lu@gmail.com>
2026-08-25 21:23:41 -0700

    mount: let a given mount option override the default
    The options from -o were placed before the ones this mount derives, so
    a volname or iosize given on the command line lost to the derived value.
    Append them last, matching the Windows adapter.

--- weed/command/mount_std.go
+++ weed/command/mount_std.go
@@ -134,7 +134,6 @@ func RunMount(option *MountOptions, umask os.FileMode) bool {
 	// mount fuse
 	fuseMountOptions := &fuse.MountOptions{
 		AllowOther:               *option.allowOthers,
-		Options:                  option.extraOptions,
 		MaxBackground:            maxBackground,
 		CongestionThreshold:      congestionThreshold,
 		MaxWrite:                 1024 * 1024 * 2,
@@ -183,6 +182,9 @@ func RunMount(option *MountOptions, umask os.FileMode) bool {
 		fuseMountOptions.Options = append(fuseMountOptions.Options, "volname="+volumeName(*option.filer, filerMountRootPath))
 		fuseMountOptions.Options = append(fuseMountOptions.Options, fmt.Sprintf("iosize=%d", ioSizeMB*1024*1024))
 	}
+	// Last, so an option given on the command line wins over the default
+	// this mount picked for it.
+	fuseMountOptions.Options = append(fuseMountOptions.Options, option.extraOptions...)
 
 	if option.writebackCache != nil {
 		fuseMountOptions.EnableWriteback = *option.writebackCache
